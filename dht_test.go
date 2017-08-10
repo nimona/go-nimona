c2cd65c7a526df75522daec0b6c5baf2738ba001
George Antoniadis <george@noodles.gr>
2017-08-10 23:32:08 +0100

    Fix addresses

--- dht_test.go
+++ dht_test.go
@@ -16,37 +16,53 @@ type dhtTestSuite struct {
 	node1 *DHTNode
 	node2 *DHTNode
 	node3 *DHTNode
+	node4 *DHTNode
+	node5 *DHTNode
 }
 
 func TestExampleTestSuite(t *testing.T) {
-	peer1 := net.Peer{ID: "a1", Addresses: []string{"0.0.0.0:21600"}}
-	peer2 := net.Peer{ID: "a2", Addresses: []string{"0.0.0.0:21601"}}
-	peer3 := net.Peer{ID: "a3", Addresses: []string{"0.0.0.0:21602"}}
-	peer4 := net.Peer{ID: "a4", Addresses: []string{"127.0.0.1:8821"}}
-	peer5 := net.Peer{ID: "a5", Addresses: []string{"0.0.0.0:21603"}}
+	peer1 := net.Peer{ID: "a1", Addresses: []string{}}
+	peer2 := net.Peer{ID: "a2", Addresses: []string{}}
+	peer3 := net.Peer{ID: "a3", Addresses: []string{}}
+	peer4 := net.Peer{ID: "a4", Addresses: []string{}}
+	peer5 := net.Peer{ID: "a5", Addresses: []string{}}
 
-	net1, err := net.NewTCPNetwork(&peer1)
+	net1, err := net.NewTCPNetwork(&peer1, 0)
 	assert.Nil(t, err)
 
-	net2, err := net.NewTCPNetwork(&peer2)
+	net2, err := net.NewTCPNetwork(&peer2, 0)
 	assert.Nil(t, err)
 
-	net3, err := net.NewTCPNetwork(&peer3)
+	net3, err := net.NewTCPNetwork(&peer3, 0)
+	assert.Nil(t, err)
+
+	net4, err := net.NewTCPNetwork(&peer4, 0)
+	assert.Nil(t, err)
+
+	net5, err := net.NewTCPNetwork(&peer5, 0)
 	assert.Nil(t, err)
 
 	rt1 := NewSimpleRoutingTable()
 	rt2 := NewSimpleRoutingTable()
 	rt3 := NewSimpleRoutingTable()
+	rt4 := NewSimpleRoutingTable()
+	rt5 := NewSimpleRoutingTable()
 
-	node1, _ := NewDHTNode([]net.Peer{peer2}, peer1, rt1, net1)
-	node2, _ := NewDHTNode([]net.Peer{peer1, peer3}, peer2, rt2, net2)
-	node3, _ := NewDHTNode([]net.Peer{peer2, peer4, peer5}, peer3, rt3, net3)
+	node1, _ := NewDHTNode([]net.Peer{}, peer1, rt1, net1)
+	node2, _ := NewDHTNode([]net.Peer{peer1}, peer2, rt2, net2)
+	node3, _ := NewDHTNode([]net.Peer{peer1}, peer3, rt3, net3)
+	node4, _ := NewDHTNode([]net.Peer{peer1}, peer4, rt4, net4)
+	node5, _ := NewDHTNode([]net.Peer{peer1}, peer5, rt5, net5)
 
 	dt := &dhtTestSuite{
 		node1: node1,
 		node2: node2,
 		node3: node3,
+		node4: node4,
+		node5: node5,
 	}
+
+	time.Sleep(time.Second * 5)
 
 	suite.Run(t, dt)
 }
