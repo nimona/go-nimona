800807a2a0c35dbf51de3d041d3df84568d96c6d
George Antoniadis <george@noodles.gr>
2018-02-04 21:43:58 +0000

    Add dialer error test

--- fabric_dialer_test.go
+++ fabric_dialer_test.go
@@ -36,8 +36,27 @@ func (suite *FabricDialerTestSuite) TestDialContextSuccess() {
 	suite.Assert().NotNil(retCtx)
 	suite.Assert().Equal(mockConn, retConn.(*conn).conn)
 	suite.Assert().Nil(retErr)
-	transport.AssertCalled(suite.T(), "DialContext", mock.Anything, addr)
 	transport.AssertCalled(suite.T(), "CanDial", addr)
+	transport.AssertCalled(suite.T(), "DialContext", mock.Anything, addr)
+}
+
+func (suite *FabricDialerTestSuite) TestDialContextFails() {
+	transport := &MockTransport{}
+	err := suite.fabric.AddTransport(transport)
+	suite.Assert().Nil(err)
+	suite.Assert().Len(suite.fabric.transports, 1)
+	suite.Assert().Equal(transport, suite.fabric.transports[0])
+
+	ctx := context.Background()
+	addrString := "some-address"
+	addr := NewAddress(addrString)
+	transport.On("CanDial", addr).Return(false, nil)
+	retCtx, retConn, retErr := suite.fabric.DialContext(ctx, addrString)
+	suite.Assert().Nil(retCtx)
+	suite.Assert().Nil(retConn)
+	suite.Assert().NotNil(retErr)
+	transport.AssertCalled(suite.T(), "CanDial", addr)
+	transport.AssertNumberOfCalls(suite.T(), "DialContext", 0)
 }
 
 func TestFabricDialerTestSuite(t *testing.T) {
