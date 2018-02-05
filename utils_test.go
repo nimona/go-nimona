b5b73ad21e58bb40bb48f04d75cd026c6a7b2cbe
George Antoniadis <george@noodles.gr>
2018-02-05 02:08:45 +0000

    Add util tests

---
+++ utils_test.go
@@ -0,0 +1,41 @@
+package fabric
+
+// Basic imports
+import (
+	"io"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// UtilsTestSuite -
+type UtilsTestSuite struct {
+	suite.Suite
+}
+
+func (suite *UtilsTestSuite) SetupTest() {
+}
+
+func (suite *UtilsTestSuite) TestReadWriteToken() {
+	reader, writter := io.Pipe()
+	payload := []byte("hello")
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		retPayload, err := ReadToken(reader)
+		suite.Assert().Nil(err)
+		suite.Assert().Equal(payload, retPayload)
+		wg.Done()
+	}()
+
+	err := WriteToken(writter, payload)
+	suite.Assert().Nil(err)
+	wg.Wait()
+}
+
+func TestUtilsTestSuite(t *testing.T) {
+	suite.Run(t, new(UtilsTestSuite))
+}
