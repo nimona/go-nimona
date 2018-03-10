4c33e3aaaebafb4fd567361ab8bb18edf2c37e50
George Antoniadis <george@noodles.gr>
2018-03-10 10:05:20 +0000

    Merge net interface and concrete

--- net/net.go
+++ net/net.go
@@ -14,6 +14,16 @@ var (
 	errNoMoreProtocols = errors.New("No more protocols")
 )
 
+// Net is our network interface for net
+type Net interface {
+	DialContext(ctx context.Context, as string) (context.Context, Conn, error)
+	AddTransport(transport Transport, protocols []Protocol) error
+	AddProtocol(protocol Protocol) error
+	GetAddresses() []string
+	CallContext(ctx context.Context, as string, extraProtocols ...Protocol) error
+	DialAndProcessWithContext(ctx context.Context, as string, extraProtocols ...Protocol) (context.Context, Conn, error)
+}
+
 // New instance of net
 func New(ctx context.Context) Net {
 	f := &nnet{
