46ec80ddad5df3dba27899baaf48eb959f0b6cc7
George Antoniadis <george@noodles.gr>
2018-04-29 17:14:35 +0100

    Rewrite dht messages to make into a more classic dht again

--- dht/message.go
+++ dht/message.go
@@ -1,23 +1,70 @@
 package dht
 
-// Message types
-const (
-	MessageTypePing string = "dht:action:ping"
-	MessageTypePut         = "dht:action:put"
-	MessageTypeGet         = "dht:action:get"
+import (
+	"github.com/nimona/go-nimona/mesh"
 )
 
-type messageGet struct {
-	OriginPeerID string            `json:"p"`
-	QueryID      string            `json:"q"`
-	Key          string            `json:"k"`
-	Labels       map[string]string `json:"l"`
+// Message types
+const (
+	PayloadTypePing string = "ping"
+	PayloadTypePong        = "pong"
+
+	PayloadTypeGetPeerInfo = "get-peer-info"
+	PayloadTypePutPeerInfo = "put-peer-info"
+
+	PayloadTypeGetProviders = "get-providers"
+	PayloadTypePutProviders = "put-providers"
+
+	PayloadTypeGetValue = "get-value"
+	PayloadTypePutValue = "put-value"
+)
+
+// User A on Peer 1 is looking to talk to User b on any peer with protocol X
+// GetProviders users/b, which returns peers 20 and 21
+// GetPeerInfo 20, GetPeerInfo21
+
+type messagePing struct {
+	RequestID string `json:"request_id,omitempty"`
+	PeerID    string `json:"peer_id"`
 }
 
-type messagePut struct {
-	OriginPeerID string            `json:"p"`
-	QueryID      string            `json:"q"`
-	Key          string            `json:"k"`
-	Value        string            `json:"v"`
-	Labels       map[string]string `json:"l"`
+type messagePong struct {
+	RequestID string `json:"request_id,omitempty"`
+	PeerID    string `json:"peer_id"`
+}
+
+type messageGetPeerInfo struct {
+	RequestID string `json:"request_id,omitempty"`
+	PeerID    string `json:"peer_id"`
+}
+
+type messagePutPeerInfo struct {
+	RequestID    string        `json:"request_id,omitempty"`
+	PeerID       string        `json:"peer_id"`
+	PeerInfo     mesh.PeerInfo `json:"peer_info"`
+	ClosestPeers []string      `json:"closest_peers"`
+}
+
+type messageGetProviders struct {
+	RequestID string `json:"request_id,omitempty"`
+	Key       string `json:"key"`
+}
+
+type messagePutProviders struct {
+	RequestID    string   `json:"request_id,omitempty"`
+	Key          string   `json:"key"`
+	PeerIDs      []string `json:"peer_ids"`
+	ClosestPeers []string `json:"closest_peers"`
+}
+
+type messageGetValue struct {
+	RequestID string `json:"request_id,omitempty"`
+	Key       string `json:"key"`
+}
+
+type messagePutValue struct {
+	RequestID    string   `json:"request_id,omitempty"`
+	Key          string   `json:"key"`
+	Value        string   `json:"value"`
+	ClosestPeers []string `json:"closest_peers"`
 }
