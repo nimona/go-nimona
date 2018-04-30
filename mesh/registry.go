e3293aea87337f792c20bf1877ba7c0df82067ea
George Antoniadis <george@noodles.gr>
2018-04-30 17:17:41 +0100

    Remove pubsub from registry

--- mesh/registry.go
+++ mesh/registry.go
@@ -1,18 +1,15 @@
 package mesh
 
 import (
-	"context"
 	"errors"
-	"strings"
-	"sync"
 	"time"
 
-	"github.com/nimona/go-nimona/mutation"
-	"go.uber.org/zap"
+	"github.com/jinzhu/copier"
 )
 
 var (
-	ErrNotKnown = errors.New("not found")
+	ErrNotKnown               = errors.New("not found")
+	ErrCannotPutLocalPeerInfo = errors.New("cannot put local peer info")
 )
 
 var (
@@ -20,138 +17,69 @@ var (
 )
 
 type Registry interface {
-	GetLocalPeerInfo() PeerInfo
-	GetPeerInfo(ctx context.Context, peerID string) (PeerInfo, error)
-	GetAllPeerInfo(ctx context.Context) ([]PeerInfo, error)
+	GetLocalPeerInfo() *PeerInfo
+	PutLocalPeerInfo(*PeerInfo) error
+	GetPeerInfo(peerID string) (*PeerInfo, error)
+	GetAllPeerInfo() ([]*PeerInfo, error)
+	PutPeerInfo(*PeerInfo) error
 	// Resolve(ctx context.Context, peerID string) (string, error)
 	// Discover(ctx context.Context, peerID, protocol string) ([]net.Address, error)
 }
 
 func NewRegisty(peerID string, ps PubSub) (Registry, error) {
-	incoming, err := ps.Subscribe("peer:.*")
-	if err != nil {
-		return nil, err
-	}
-
 	reg := &registry{
-		localPeer: PeerInfo{
+		localPeer: &PeerInfo{
 			ID:        peerID,
 			Protocols: map[string][]string{},
 		},
-		incoming:  incoming,
-		protocols: sync.Map{},
+		peers: map[string]*PeerInfo{},
 	}
-
-	logger, _ := zap.NewDevelopment()
-
-	go func() {
-		for event := range reg.incoming {
-			switch mut := event.(type) {
-			case mutation.PeerProtocolDiscovered:
-				logger.Info(
-					"Discovered protocol for peer",
-					zap.String("peer", mut.PeerID),
-					zap.String("protocol", mut.ProtocolName),
-					zap.String("address", mut.ProtocolAddress),
-				)
-				protocol := &peerInfoProtocol{
-					PeerID:      mut.PeerID,
-					Name:        mut.ProtocolName,
-					Address:     mut.ProtocolAddress,
-					LastUpdated: time.Now(),
-					Pinned:      mut.Pinned,
-				}
-				reg.protocols.Store(protocol.Hash(), protocol)
-			}
-		}
-	}()
 
 	return reg, nil
 }
 
 type registry struct {
-	protocols sync.Map
-	localPeer PeerInfo
-	incoming  chan interface{}
+	peers     map[string]*PeerInfo
+	localPeer *PeerInfo
 }
 
-func (reg *registry) GetLocalPeerInfo() PeerInfo {
-	return reg.localPeer
+func (reg *registry) PutPeerInfo(peerInfo *PeerInfo) error {
+	if reg.localPeer.ID == peerInfo.ID {
+		return ErrCannotPutLocalPeerInfo
+	}
+
+	reg.peers[peerInfo.ID] = peerInfo
+	return nil
 }
 
-func (reg *registry) GetPeerInfo(ctx context.Context, peerID string) (PeerInfo, error) {
-	peerInfo := PeerInfo{
-		ID:        peerID,
-		Protocols: map[string][]string{},
-	}
-
-	protocols := []*peerInfoProtocol{}
-	reg.protocols.Range(func(k, v interface{}) bool {
-		protocol := v.(*peerInfoProtocol)
-		if strings.HasPrefix(k.(string), peerID) {
-			protocols = append(protocols, protocol)
-		}
-		return true
-	})
-
-	for _, protocolInfo := range protocols {
-		if protocolInfo.PeerID != peerID {
-			continue
-		}
-
-		name := protocolInfo.Name
-		if _, ok := peerInfo.Protocols[name]; !ok {
-			peerInfo.Protocols[name] = []string{}
-		}
-
-		expired := protocolInfo.LastUpdated.Add(peerInfoExpireAfter).Before(time.Now())
-		if protocolInfo.Pinned == false && expired {
-			continue
-		}
-
-		peerInfo.Protocols[name] = append(peerInfo.Protocols[name], protocolInfo.Address)
-	}
-
-	return peerInfo, nil
+func (reg *registry) GetLocalPeerInfo() *PeerInfo {
+	newPeerInfo := &PeerInfo{}
+	copier.Copy(newPeerInfo, reg.localPeer)
+	return newPeerInfo
 }
 
-func (reg *registry) GetAllPeerInfo(ctx context.Context) ([]PeerInfo, error) {
-	peers := map[string]*PeerInfo{}
+func (reg *registry) PutLocalPeerInfo(peerInfo *PeerInfo) error {
+	reg.localPeer = peerInfo
+	return nil
+}
 
-	protocols := []*peerInfoProtocol{}
-	reg.protocols.Range(func(k, v interface{}) bool {
-		protocol := v.(*peerInfoProtocol)
-		protocols = append(protocols, protocol)
-		return true
-	})
-
-	for _, protocolInfo := range protocols {
-		peerInfo, ok := peers[protocolInfo.PeerID]
-		if !ok {
-			peerInfo = &PeerInfo{
-				ID:        protocolInfo.PeerID,
-				Protocols: map[string][]string{},
-			}
-			peers[protocolInfo.PeerID] = peerInfo
-		}
-
-		name := protocolInfo.Name
-		if _, ok := peerInfo.Protocols[name]; !ok {
-			peerInfo.Protocols[name] = []string{}
-		}
-
-		expired := protocolInfo.LastUpdated.Add(peerInfoExpireAfter).Before(time.Now())
-		if protocolInfo.Pinned == false && expired {
-			continue
-		}
-
-		peerInfo.Protocols[name] = append(peerInfo.Protocols[name], protocolInfo.Address)
+func (reg *registry) GetPeerInfo(peerID string) (*PeerInfo, error) {
+	peerInfo, ok := reg.peers[peerID]
+	if !ok {
+		return nil, ErrNotKnown
 	}
 
-	peerInfos := []PeerInfo{}
-	for _, pi := range peers {
-		peerInfos = append(peerInfos, *pi)
-	}
+	newPeerInfo := &PeerInfo{}
+	copier.Copy(newPeerInfo, peerInfo)
+	return newPeerInfo, nil
+}
 
-	return peerInfos, nil
+func (reg *registry) GetAllPeerInfo() ([]*PeerInfo, error) {
+	newPeerInfos := []*PeerInfo{}
+	for _, peerInfo := range reg.peers {
+		newPeerInfo := &PeerInfo{}
+		copier.Copy(newPeerInfo, peerInfo)
+		newPeerInfos = append(newPeerInfos, newPeerInfo)
+	}
+	return newPeerInfos, nil
 }
