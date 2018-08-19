d09b7b0fc5fa42958389aba900f8e66cb190fff3
Paris Goudas <decimal@darkstar.gr>
2018-08-20 00:21:34 +0100

    Refactor telemetry processor

--- telemetry/collector_influx.go
+++ telemetry/collector_influx.go
@@ -5,16 +5,23 @@ import (
 	"sync"
 	"time"
 
+	"github.com/nimona/go-nimona/log"
+
+	"go.uber.org/zap"
+
 	"context"
 
 	"github.com/influxdata/influxdb/client/v2"
 )
 
 type InfluxCollector struct {
-	client client.Client
-	points client.BatchPoints
-	input  chan Collectable
-	lock   *sync.RWMutex
+	client      client.Client
+	input       chan Collectable
+	lock        *sync.RWMutex
+	logger      *zap.Logger
+	batchSize   int
+	timeout     time.Duration
+	batchConfig client.BatchPointsConfig
 }
 
 const databaseName = "nimona_metrics"
@@ -26,20 +33,31 @@ func NewInfluxCollector(user, pass, addr string) (Collector, error) {
 		Password: pass,
 	})
 	if err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 
 	input := make(chan Collectable)
 
-	// TODO search for database if not there create it?
-
 	ic := &InfluxCollector{
 		client: c,
 		lock:   &sync.RWMutex{},
 		input:  input,
+		logger: log.Logger(context.Background()).Named("collector_influx"),
+		// TODO fix the point aggregation and find a sane batch size
+		batchSize: 1,
+		batchConfig: client.BatchPointsConfig{
+			Database:  databaseName,
+			Precision: "ns",
+		},
+		timeout: 1000 * time.Millisecond,
 	}
 
+	// Create the database
+	if err := ic.createDB(databaseName); err != nil {
+		return nil, err
+	}
+
+	// Initialize the processor
 	go ic.processor(context.Background(), input)
 
 	return ic, nil
@@ -52,17 +70,12 @@ func (ic *InfluxCollector) Collect(event Collectable) error {
 
 func (ic *InfluxCollector) processor(ctx context.Context,
 	input <-chan Collectable) {
-	// Size of the write batch
-	batchSize := 1000
-	batchConfig := client.BatchPointsConfig{
-		Database: databaseName,
-	}
 
 	// Create a new point batch
-	bp, err := client.NewBatchPoints(batchConfig)
+	bp, err := client.NewBatchPoints(ic.batchConfig)
 	if err != nil {
-		// TODO handle error
-		// close channel
+		ic.logger.Error("Failed to create new batch points", zap.Error(err))
+		return
 	}
 
 	timeout := make(chan bool)
@@ -70,7 +83,7 @@ func (ic *InfluxCollector) processor(ctx context.Context,
 	// Goroutine that send timeout signal every xx ms
 	go func(timeout chan bool) {
 		for {
-			time.Sleep(1000 * time.Millisecond)
+			time.Sleep(ic.timeout)
 			timeout <- true
 		}
 	}(timeout)
@@ -82,41 +95,46 @@ func (ic *InfluxCollector) processor(ctx context.Context,
 			pt, err := client.NewPoint(event.Collection(), tags,
 				event.Measurements())
 			if err != nil {
-				// TODO log error
-				fmt.Println(err)
+				ic.logger.Error("Failed to create point", zap.Error(err))
 			}
 			bp.AddPoint(pt)
 		case <-timeout:
-			err := ic.client.Write(bp)
-			if err != nil {
-				// TODO log error
-				fmt.Println(err)
+			if err := ic.writePoints(bp); err != nil {
+				ic.logger.Error("Failed to write points", zap.Error(err))
 			}
-			fmt.Println("Wrote points from timeout: ", len(bp.Points()))
-			bp, _ = client.NewBatchPoints(batchConfig)
 		case <-ctx.Done():
-			err := ic.client.Write(bp)
-			if err != nil {
-				// TODO log error
-				fmt.Println(err)
+			if err := ic.writePoints(bp); err != nil {
+				ic.logger.Error("Failed to write points", zap.Error(err))
 			}
 			break
-			// TODO dump write
 		}
 
-		if len(bp.Points()) >= batchSize {
-			err := ic.client.Write(bp)
-			if err != nil {
-				// TODO log error
-				fmt.Println(err)
+		if len(bp.Points()) >= ic.batchSize {
+			if err := ic.writePoints(bp); err != nil {
+				ic.logger.Error("Failed to write points", zap.Error(err))
 			}
-			fmt.Println("Wrote points from size: ", len(bp.Points()))
-
-			bp, _ = client.NewBatchPoints(batchConfig)
-			// TODO check error
 		}
 	}
 }
 
-// collect just adds points to a struct
-// a go routine checks if there are
+func (ic *InfluxCollector) createDB(db string) error {
+	_, err := ic.client.Query(client.Query{
+		Command: fmt.Sprintf("CREATE DATABASE %s", db),
+	})
+	if err != nil {
+		ic.logger.Error("Failed to create database", zap.Error(err))
+		return err
+	}
+
+	return nil
+}
+
+func (ic *InfluxCollector) writePoints(bp client.BatchPoints) error {
+	err := ic.client.Write(bp)
+	if err != nil {
+		return err
+	}
+
+	bp, _ = client.NewBatchPoints(ic.batchConfig)
+	return nil
+}
