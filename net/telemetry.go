346ccb50370092a56f7131a6403ef7f8ff25afad
Paris Goudas <paris@darkstar.gr>
2018-09-05 22:49:21 +0100

    Disable net telemetry

--- net/telemetry.go
+++ net/telemetry.go
@@ -1,46 +1,25 @@
 package net
 
-import (
-	"fmt"
+// // SendEvent sends an event
+// func SendEvent(event telemetry.Collectable) {
 
-	blocks "github.com/nimona/go-nimona/blocks"
-	"github.com/nimona/go-nimona/telemetry"
-)
+// }
 
-func init() {
-	blocks.RegisterContentType("nimona.telemetry.connection",
-		telemetry.ConnectionEvent{})
-	blocks.RegisterContentType("nimona.telemetry.block",
-		telemetry.BlockEvent{})
-}
+// // SendConnectionEvent sends a connection event
+// func SendConnectionEvent(outgoing bool) {
+// 	SendEvent(&telemetry.ConnectionEvent{
+// 		Outgoing: outgoing,
+// 	})
+// }
 
-// SendEvent sends an event
-func SendEvent(event telemetry.Collectable) {
-	if telemetry.DefaultCollector == nil {
-		return
-	}
-	// TODO Wrap or extend event with version and other static information
-	// TODO properly log error
-	if err := telemetry.DefaultCollector.Collect(event); err != nil {
-		fmt.Println("telemetry error", err)
-	}
-}
-
-// SendConnectionEvent sends a connection event
-func SendConnectionEvent(outgoing bool) {
-	SendEvent(&telemetry.ConnectionEvent{
-		Outgoing: outgoing,
-	})
-}
-
-// SendBlockEvent sends a connection event
-func SendBlockEvent(outgoing bool, contentType string, recipients,
-	payloadSize, blockSize int) {
-	SendEvent(&telemetry.BlockEvent{
-		Outgoing:    outgoing,
-		ContentType: contentType,
-		Recipients:  recipients,
-		PayloadSize: payloadSize,
-		BlockSize:   blockSize,
-	})
-}
+// // SendBlockEvent sends a connection event
+// func SendBlockEvent(outgoing bool, contentType string, recipients,
+// 	payloadSize, blockSize int) {
+// 	SendEvent(&telemetry.BlockEvent{
+// 		Outgoing:    outgoing,
+// 		ContentType: contentType,
+// 		Recipients:  recipients,
+// 		PayloadSize: payloadSize,
+// 		BlockSize:   blockSize,
+// 	})
+// }
