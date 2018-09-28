3a196b642e9e5d29055c346afe0c7461dd13f1a0
George Antoniadis <george@noodles.gr>
2018-09-28 06:38:23 +0100

    Fix test race

--- net/exchange_test.go
+++ net/exchange_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/assert"
 
 	"nimona.io/go/net"
 	"nimona.io/go/peers"
@@ -17,18 +17,14 @@ import (
 	"nimona.io/go/storage"
 )
 
-type exchangeTestSuite struct {
-	suite.Suite
-}
-
-func (suite *exchangeTestSuite) TestSendSuccess() {
-	_, p1, _, w1, r1 := suite.newPeer()
-	_, p2, k2, w2, r2 := suite.newPeer()
+func TestSendSuccess(t *testing.T) {
+	_, p1, _, w1, r1 := newPeer(t)
+	_, p2, k2, w2, r2 := newPeer(t)
 
 	err := r2.PutPeerInfo(p1)
-	suite.NoError(err)
+	assert.NoError(t, err)
 	err = r1.PutPeerInfo(p2)
-	suite.NoError(err)
+	assert.NoError(t, err)
 
 	time.Sleep(time.Second)
 
@@ -53,17 +49,17 @@ func (suite *exchangeTestSuite) TestSendSuccess() {
 	w2BlockHandled := false
 
 	w1.Handle("test.nimona.io/dummy", func(block *primitives.Block) error {
-		suite.Equal(k2.GetPublicKey().Thumbprint(), block.Signature.Key.Thumbprint())
-		block.Signature = nil
-		exPayload1.Signature = nil
-		suite.Equal(exPayload1, block)
+		assert.Equal(t, k2.GetPublicKey().Thumbprint(), block.Signature.Key.Thumbprint())
+		assert.Equal(t, exPayload1.Type, block.Type)
+		assert.Equal(t, exPayload1.Annotations, block.Annotations)
+		assert.Equal(t, exPayload1.Payload, block.Payload)
 		w1BlockHandled = true
 		wg.Done()
 		return nil
 	})
 
 	w2.Handle("test.nimona.**", func(block *primitives.Block) error {
-		suite.Equal(exPayload2, block)
+		assert.Equal(t, exPayload2, block)
 		w2BlockHandled = true
 		wg.Done()
 		return nil
@@ -72,27 +68,27 @@ func (suite *exchangeTestSuite) TestSendSuccess() {
 	ctx := context.Background()
 
 	err = w2.Send(ctx, exPayload1, p1.Signature.Key, primitives.SignWith(k2))
-	suite.NoError(err)
+	assert.NoError(t, err)
 
 	time.Sleep(time.Second)
 
 	// TODO should be able to send not signed
 	err = w1.Send(ctx, exPayload2, p2.Signature.Key)
-	suite.NoError(err)
+	assert.NoError(t, err)
 
 	wg.Wait()
 
-	suite.True(w1BlockHandled)
-	suite.True(w2BlockHandled)
+	assert.True(t, w1BlockHandled)
+	assert.True(t, w2BlockHandled)
 }
 
-// func (suite *exchangeTestSuite) TestRelayedSendSuccess() {
-// 	portR, pR, wR, rR := suite.newPeer()
+// func (assert *exchangeTestSuite)t,  TestRelayedSendSuccess() {
+// 	portR, pR, wR, rR := assert.newPeer(t, )
 // 	pRs := pR.Block()
 // 	pRs.Addresses = []string{fmt.Sprintf("tcp:127.0.0.1:%d", portR)}
 
-// 	_, p1, w1, r1 := suite.newPeer()
-// 	_, p2, w2, r2 := suite.newPeer()
+// 	_, p1, w1, r1 := assert.newPeer(t, )
+// 	_, p2, w2, r2 := assert.newPeer(t, )
 
 // 	r1.PutPeerInfoFromBlock(&pRs)
 // 	r2.PutPeerInfoFromBlock(&pRs)
@@ -126,8 +122,8 @@ func (suite *exchangeTestSuite) TestSendSuccess() {
 // 	w1.Handle("foo", func(block *nprimitives.Block) error {
 // 		decPayload := map[string]string{}
 // 		err := block.DecodePayload(&decPayload)
-// 		suite.NoError(err)
-// 		suite.Equal(payload, decPayload)
+// 		assert.NoError(t, err)
+// 		assert.Equal(t, payload, decPayload)
 // 		w1BlockHandled = true
 // 		wg.Done()
 // 		return nil
@@ -136,8 +132,8 @@ func (suite *exchangeTestSuite) TestSendSuccess() {
 // 	w2.Handle("foo", func(block *nprimitives.Block) error {
 // 		decPayload := map[string]string{}
 // 		err := block.DecodePayload(&decPayload)
-// 		suite.NoError(err)
-// 		suite.Equal(payload, decPayload)
+// 		assert.NoError(t, err)
+// 		assert.Equal(t, payload, decPayload)
 // 		w2BlockHandled = true
 // 		wg.Done()
 // 		return nil
@@ -146,33 +142,29 @@ func (suite *exchangeTestSuite) TestSendSuccess() {
 // 	ctx := context.Background()
 
 // 	block, err := net.NewBlock("foo.bar", []string{p1.ID}, payload)
-// 	suite.NoError(err)
+// 	assert.NoError(t, err)
 // 	err = w2.Send(ctx, block)
-// 	suite.NoError(err)
+// 	assert.NoError(t, err)
 
 // 	time.Sleep(time.Second)
 
 // 	block, err = net.NewBlock("foo.bar", []string{p2.ID}, payload)
-// 	suite.NoError(err)
+// 	assert.NoError(t, err)
 // 	err = w1.Send(ctx, block)
-// 	suite.NoError(err)
+// 	assert.NoError(t, err)
 
 // 	wg.Wait()
 
-// 	suite.True(w1BlockHandled)
-// 	suite.True(w2BlockHandled)
+// 	assert.True(t, w1BlockHandled)
+// 	assert.True(t, w2BlockHandled)
 // }
 
-func (suite *exchangeTestSuite) newPeer() (int, *peers.PeerInfo, *primitives.Key, net.Exchange, *peers.AddressBook) {
+func newPeer(t *testing.T) (int, *peers.PeerInfo, *primitives.Key, net.Exchange, *peers.AddressBook) {
 	td, _ := ioutil.TempDir("", "nimona-test-net")
 	ab, _ := peers.NewAddressBook(td)
 	storagePath := path.Join(td, "storage")
 	dpr := storage.NewDiskStorage(storagePath)
 	wre, err := net.NewExchange(ab, dpr, fmt.Sprintf("0.0.0.0:%d", 0))
-	suite.NoError(err)
+	assert.NoError(t, err)
 	return 0, ab.GetLocalPeerInfo(), ab.GetLocalPeerKey(), wre, ab
-}
-
-func TestExchangeTestSuite(t *testing.T) {
-	suite.Run(t, new(exchangeTestSuite))
 }
