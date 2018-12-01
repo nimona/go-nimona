bb8340c4b665d2ff8b413045c9117a9c3db5c04e
George Antoniadis <george@noodles.gr>
2018-12-01 13:42:48 +0000

    Simplify object

--- encoding/object.go
+++ encoding/object.go
@@ -12,15 +12,7 @@ type Typed interface {
 }
 
 // Object for everything f12n
-type Object struct {
-	data map[string]interface{}
-	// bytes []byte
-	// ctx       string
-	// policy    *Object
-	// authority *Object
-	// signer    *Object
-	// signature *Object
-}
+type Object map[string]interface{}
 
 // NewObjectFromBytes returns an object from a cbor byte stream
 func NewObjectFromBytes(b []byte) (*Object, error) {
@@ -30,16 +22,12 @@ func NewObjectFromBytes(b []byte) (*Object, error) {
 	}
 
 	o := NewObjectFromMap(m)
-	// o.bytes = b
 	return o, nil
 }
 
 // NewObject returns an object from a map
 func NewObject() *Object {
-	o := &Object{
-		data: map[string]interface{}{},
-	}
-	return o
+	return &Object{}
 }
 
 // NewObjectFromMap returns an object from a map
@@ -50,26 +38,26 @@ func NewObjectFromMap(m map[string]interface{}) *Object {
 }
 
 // FromMap inits the object from a map
-func (o *Object) FromMap(m map[string]interface{}) {
+func (o Object) FromMap(m map[string]interface{}) {
 	for k, v := range m {
 		o.SetRaw(k, v)
 	}
 }
 
 // Hash returns the object's hash
-func (o *Object) Hash() []byte {
-	return Hash(o)
+func (o Object) Hash() []byte {
+	return Hash(&o)
 }
 
 // HashBase58 returns the object's hash base58 encoded
-func (o *Object) HashBase58() string {
-	return base58.Encode(Hash(o))
+func (o Object) HashBase58() string {
+	return base58.Encode(Hash(&o))
 }
 
 // Map returns the object as a map
-func (o *Object) Map() map[string]interface{} {
+func (o Object) Map() map[string]interface{} {
 	m := map[string]interface{}{}
-	for k, v := range o.data {
+	for k, v := range o {
 		// TODO check the type hint first maybe?
 		if o, ok := v.(*Object); ok {
 			m[k] = o.Map()
@@ -81,7 +69,7 @@ func (o *Object) Map() map[string]interface{} {
 }
 
 // GetType returns the object's type
-func (o *Object) GetType() string {
+func (o Object) GetType() string {
 	if v, ok := o.GetRaw("@ctx:s").(string); ok {
 		return v
 	}
@@ -89,12 +77,12 @@ func (o *Object) GetType() string {
 }
 
 // SetType sets the object's type
-func (o *Object) SetType(v string) {
+func (o Object) SetType(v string) {
 	o.SetRaw("@ctx:s", v)
 }
 
 // GetSignature returns the object's signature, or nil
-func (o *Object) GetSignature() *Object {
+func (o Object) GetSignature() *Object {
 	if v, ok := o.GetRaw("@sig:O").(*Object); ok {
 		return v
 	}
@@ -102,12 +90,12 @@ func (o *Object) GetSignature() *Object {
 }
 
 // SetSignature sets the object's signature
-func (o *Object) SetSignature(v *Object) {
+func (o Object) SetSignature(v *Object) {
 	o.SetRaw("@sig:O", v)
 }
 
 // GetAuthorityKey returns the object's creator, or nil
-func (o *Object) GetAuthorityKey() *Object {
+func (o Object) GetAuthorityKey() *Object {
 	if v, ok := o.GetRaw("@authority:O").(*Object); ok {
 		return v
 	}
@@ -115,12 +103,12 @@ func (o *Object) GetAuthorityKey() *Object {
 }
 
 // SetAuthorityKey sets the object's creator
-func (o *Object) SetAuthorityKey(v *Object) {
+func (o Object) SetAuthorityKey(v *Object) {
 	o.SetRaw("@authority:O", v)
 }
 
 // GetSignerKey returns the object's signer, or nil
-func (o *Object) GetSignerKey() *Object {
+func (o Object) GetSignerKey() *Object {
 	if v, ok := o.GetRaw("@signer:O").(*Object); ok {
 		return v
 	}
@@ -128,12 +116,12 @@ func (o *Object) GetSignerKey() *Object {
 }
 
 // SetSignerKey sets the object's signer
-func (o *Object) SetSignerKey(v *Object) {
+func (o Object) SetSignerKey(v *Object) {
 	o.SetRaw("@signer:O", v)
 }
 
 // GetPolicy returns the object's policy, or nil
-func (o *Object) GetPolicy() *Object {
+func (o Object) GetPolicy() *Object {
 	if v, ok := o.GetRaw("@policy:O").(*Object); ok {
 		return v
 	}
@@ -141,15 +129,15 @@ func (o *Object) GetPolicy() *Object {
 }
 
 // SetPolicy sets the object's policy
-func (o *Object) SetPolicy(v *Object) {
+func (o Object) SetPolicy(v *Object) {
 	o.SetRaw("@policy:O", v)
 }
 
 // GetRaw -
-func (o *Object) GetRaw(lk string) interface{} {
+func (o Object) GetRaw(lk string) interface{} {
 	// TODO(geoah) do we need to verify type if k has hint?
 	lk = getCleanKeyName(lk)
-	for k, v := range o.data {
+	for k, v := range o {
 		if getCleanKeyName(k) == lk {
 			return v
 		}
@@ -159,7 +147,7 @@ func (o *Object) GetRaw(lk string) interface{} {
 }
 
 // SetRaw -
-func (o *Object) SetRaw(k string, v interface{}) {
+func (o Object) SetRaw(k string, v interface{}) {
 	// add type hint if not already set
 	et := getFullType(k)
 	if et == "" {
@@ -173,7 +161,7 @@ func (o *Object) SetRaw(k string, v interface{}) {
 	}
 
 	// add the attribute in the data map
-	o.data[k] = v
+	o[k] = v
 
 	// check if this is a magic attribute and set it
 	ck := getCleanKeyName(k)
@@ -183,44 +171,44 @@ func (o *Object) SetRaw(k string, v interface{}) {
 		if !ok {
 			panic(fmt.Errorf("invalid type %T for @ctx", v))
 		}
-		o.data["@ctx:s"] = t
+		o["@ctx:s"] = t
 	case "@policy:O":
 		if oi, ok := v.(*Object); ok {
-			o.data["@policy:O"] = oi
+			o["@policy:O"] = oi
 		} else if oi, ok := v.(objectable); ok {
-			o.data["@policy:O"] = oi.ToObject()
+			o["@policy:O"] = oi.ToObject()
 		} else if m, ok := v.(map[string]interface{}); ok {
-			o.data["@policy:O"] = NewObjectFromMap(m)
+			o["@policy:O"] = NewObjectFromMap(m)
 		} else {
 			panic(fmt.Errorf("invalid type %T for @policy", v))
 		}
 	case "@authority:O":
 		if oi, ok := v.(*Object); ok {
-			o.data["@authority:O"] = oi
+			o["@authority:O"] = oi
 		} else if oi, ok := v.(objectable); ok {
-			o.data["@authority:O"] = oi.ToObject()
+			o["@authority:O"] = oi.ToObject()
 		} else if m, ok := v.(map[string]interface{}); ok {
-			o.data["@authority:O"] = NewObjectFromMap(m)
+			o["@authority:O"] = NewObjectFromMap(m)
 		} else {
 			panic(fmt.Errorf("invalid type %T for @authority", v))
 		}
 	case "@signer:O":
 		if oi, ok := v.(*Object); ok {
-			o.data["@signer:O"] = oi
+			o["@signer:O"] = oi
 		} else if oi, ok := v.(objectable); ok {
-			o.data["@signer:O"] = oi.ToObject()
+			o["@signer:O"] = oi.ToObject()
 		} else if m, ok := v.(map[string]interface{}); ok {
-			o.data["@signer:O"] = NewObjectFromMap(m)
+			o["@signer:O"] = NewObjectFromMap(m)
 		} else {
 			panic(fmt.Errorf("invalid type %T for @signer", v))
 		}
 	case "@sig:O":
 		if oi, ok := v.(*Object); ok {
-			o.data["@sig:O"] = oi
+			o["@sig:O"] = oi
 		} else if oi, ok := v.(objectable); ok {
-			o.data["@sig:O"] = oi.ToObject()
+			o["@sig:O"] = oi.ToObject()
 		} else if m, ok := v.(map[string]interface{}); ok {
-			o.data["@sig:O"] = NewObjectFromMap(m)
+			o["@sig:O"] = NewObjectFromMap(m)
 		} else {
 			panic(fmt.Errorf("invalid type %T for @sig", v))
 		}
