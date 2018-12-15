f9784b202625b37312b0f10a869b73bf049e71f6
Paris Goudas <decimal@darkstar.gr>
2018-12-15 00:51:46 +0000

    Rename auth header to Authorization

--- api/middleware_auth.go
+++ api/middleware_auth.go
@@ -8,7 +8,7 @@ import (
 
 func (api *API) TokenAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if api.token != c.Request.Header.Get("token") {
+		if api.token != c.Request.Header.Get("Authorization") {
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
