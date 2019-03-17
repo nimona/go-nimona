59181c1854e06833ba91e3f50186ba771175156b
Paris Goudas <decimal@darkstar.gr>
2019-03-17 21:51:03 +0000

    Add ErrNonce in error file

--- pkg/net/errors.go
+++ pkg/net/errors.go
@@ -11,7 +11,9 @@ var (
 	ErrNotForUs = errors.New("object not for us")
 	// ErrMissingKey when a key is not passed
 	ErrMissingKey = errors.New("missing key")
-	// ErrPrivateKeyRequired when a key is not an ECDSA key
+	// ErrECDSAPrivateKeyRequired when a key is not an ECDSA key
 	ErrECDSAPrivateKeyRequired = errors.New(
 		"network currently requires an ecdsa private key")
+	// ErrNonce is when the nonce does not match
+	ErrNonce = errors.New("nonce does not match")
 )
