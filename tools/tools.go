a2ffcec2c38c8a7def8cb1243fbf8030ceb4dd09
George Antoniadis <george@noodles.gr>
2019-04-14 20:35:15 +0100

    chore(tools): add tools package for managing versions with go mod

---
+++ tools/tools.go
@@ -0,0 +1,13 @@
+// +build tools
+
+package tools
+
+import (
+	// See README.md before touching this file
+	_ "github.com/cheekybits/genny"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/shurcooL/vfsgen/cmd/vfsgendev"
+	_ "github.com/vektra/mockery/cmd/mockery"
+)
