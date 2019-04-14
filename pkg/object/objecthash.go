2d4395713134b82116c22e456030aa70a4045cac
George Antoniadis <george@noodles.gr>
2019-04-14 20:43:56 +0100

    fix(object/hash): fix type hinting, ignore understore prefixed keys

--- pkg/object/objecthash.go
+++ pkg/object/objecthash.go
@@ -14,15 +14,32 @@ import (
 // as TJSON instead.
 // TODO add redaction
 func ObjectHash(o *Object) ([]byte, error) {
-	return objecthash(o, true)
+	return objecthash(o.ToMap(), true)
 }
 
-func objecthash(o *Object, skipSig bool) ([]byte, error) {
-	m := o.ToMap()
+func hintsFromKey(k string) []TypeHint {
+	ps := strings.Split(k, ":")
+	if len(ps) == 1 {
+		return nil
+	}
+	hs := []TypeHint{}
+	kh := strings.Replace(ps[1], "<", "", -1)
+	kh = strings.Replace(kh, ">", "", -1)
+	for _, sh := range kh {
+		hs = append(hs, GetTypeHint(string(sh)))
+	}
+	return hs
+}
+
+func objecthash(m map[string]interface{}, skipSig bool) ([]byte, error) {
 	b := []byte{}
 	ks := []string{}
 	for k := range m {
+		if len(k) > 0 && k[0] == '_' {
+			continue
+		}
 		// TODO(geoah) is there a better way of doing this?
+		// TODO(geoah) better question is, do we need @?
 		if k == "@" {
 			continue
 		}
@@ -35,10 +52,12 @@ func objecthash(o *Object, skipSig bool) ([]byte, error) {
 	x := map[string]interface{}{}
 	for _, k := range ks {
 		v := m[k]
-		hv := hashValue(v)
+		ts := hintsFromKey(k)
+		hv := hashValueAs(k, v, ts...)
 		if hv == nil {
 			continue
 		}
+		// fmt.Println("hashing value for", k, "as", fmt.Sprintf("%x", hv))
 		hk := hash(HintString, []byte(k))
 		b = append(b, hk...)
 		b = append(b, hv...)
@@ -59,19 +78,18 @@ func hash(p TypeHint, b []byte) []byte {
 	return h.Sum(nil)
 }
 
-func hashValue(o interface{}) []byte {
+func hashValueAs(k string, o interface{}, ts ...TypeHint) []byte {
 	v := reflect.ValueOf(o)
 	t := reflect.TypeOf(o)
-	switch t.Kind() {
-	case reflect.Invalid: // nil
-		// return hash(HintNil, []byte{})
+
+	if len(ts) == 0 {
 		return nil
-	case reflect.Slice, reflect.Array:
+	}
+
+	switch ts[0] {
+	case HintArray:
 		if v.Len() == 0 {
 			return nil
-		}
-		if t.Elem() == reflect.TypeOf(byte(0)) {
-			return hash(HintData, o.([]byte))
 		}
 		vs := []interface{}{}
 		for i := 0; i < v.Len(); i++ {
@@ -79,17 +97,63 @@ func hashValue(o interface{}) []byte {
 		}
 		b := []byte{}
 		for _, iv := range vs {
-			b = append(b, hashValue(iv)...)
+			b = append(b, hashValueAs(k, iv, ts[1:]...)...)
 		}
+		// TODO(geoah) hint SHOULD NOT be array, but array + inner hint
 		return hash(HintArray, b)
-	case reflect.String:
+	case HintString:
 		if o.(string) == "" {
 			return nil
 		}
 		return hash(HintString, []byte(o.(string)))
-	case reflect.Struct:
-		panic("structs are not currently supported")
-	case reflect.Map:
+	case HintData:
+		switch t.Kind() {
+		case reflect.String:
+			panic("handle baseXX encoded data")
+		case reflect.Slice:
+			bo := make([]byte, v.Len())
+			switch t.Elem().Kind() {
+			case reflect.Int,
+				reflect.Int8,
+				reflect.Int16,
+				reflect.Int32,
+				reflect.Int64:
+				for i := 0; i < v.Len(); i++ {
+					bo[i] = uint8(v.Index(i).Int())
+				}
+				return hash(HintData, bo)
+			case reflect.Uint,
+				reflect.Uint8,
+				reflect.Uint16,
+				reflect.Uint32,
+				reflect.Uint64:
+				for i := 0; i < v.Len(); i++ {
+					bo[i] = uint8(v.Index(i).Uint())
+				}
+				return hash(HintData, bo)
+			case reflect.Interface:
+				for i := 0; i < v.Len(); i++ {
+					iv := v.Index(i).Interface()
+					switch iv.(type) {
+					case uint8:
+						bo[i] = iv.(uint8)
+					case uint64:
+						bo[i] = uint8(iv.(uint64))
+					case float64:
+						bo[i] = uint8(iv.(float64))
+					default:
+						panic("data should be some sort of number array, was " +
+							t.Elem().Kind().String())
+					}
+				}
+				return hash(HintData, bo)
+			default:
+				panic("data should be some sort of number array, was " +
+					t.Elem().Kind().String())
+			}
+		}
+		return hash(HintData, o.([]byte))
+	case HintObject:
 		m, ok := o.(map[string]interface{})
 		if !ok {
 			panic("hashing only supports map[string]interface{}")
@@ -97,30 +161,137 @@ func hashValue(o interface{}) []byte {
 		if len(m) == 0 {
 			return nil
 		}
-		o := FromMap(m)
-		h, err := objecthash(o, false)
+		h, err := objecthash(m, false)
 		if err != nil {
 			panic("hashing error: " + err.Error())
 		}
 		return h
-	case reflect.Float32, reflect.Float64:
-		nf, err := hashFloat(v.Float())
-		if err != nil {
-			panic(err)
+	case HintFloat:
+		switch t.Kind() {
+		case reflect.Float32,
+			reflect.Float64:
+			nf, err := hashFloat(v.Float())
+			if err != nil {
+				panic(err)
+			}
+			return hash(HintFloat, nf)
+		case reflect.Int,
+			reflect.Int8,
+			reflect.Int16,
+			reflect.Int32,
+			reflect.Int64:
+			nf, err := hashFloat(float64(v.Int()))
+			if err != nil {
+				panic(err)
+			}
+			return hash(HintFloat, nf)
+		case reflect.Uint,
+			reflect.Uint8,
+			reflect.Uint16,
+			reflect.Uint32,
+			reflect.Uint64:
+			nf, err := hashFloat(float64(v.Uint()))
+			if err != nil {
+				panic(err)
+			}
+			return hash(HintFloat, nf)
 		}
-		return hash(HintFloat, []byte(nf))
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return hash(HintInt, []byte(fmt.Sprintf("%d", v.Int())))
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return hash(HintUint, []byte(fmt.Sprintf("%d", v.Uint())))
-	case reflect.Bool:
+	case HintInt:
+		switch t.Kind() {
+		case reflect.Float32,
+			reflect.Float64:
+			return hash(HintInt, []byte(fmt.Sprintf("%d", int64(v.Float()))))
+		case reflect.Int,
+			reflect.Int8,
+			reflect.Int16,
+			reflect.Int32,
+			reflect.Int64:
+			return hash(HintInt, []byte(fmt.Sprintf("%d", v.Int())))
+		case reflect.Uint,
+			reflect.Uint8,
+			reflect.Uint16,
+			reflect.Uint32,
+			reflect.Uint64:
+			return hash(HintInt, []byte(fmt.Sprintf("%d", int64(v.Uint()))))
+		}
+	case HintUint:
+		return hash(HintUint, []byte(fmt.Sprintf("%d", o)))
+	case HintBool:
 		if v.Bool() {
 			return hash(HintBool, []byte{1})
 		}
 		return hash(HintBool, []byte{0})
 	}
-	panic("hash: unsupported type " + v.String() + " -- " + fmt.Sprintf("%#v", o))
+	panic(
+		fmt.Sprintf("hash: unsupported type %s (%s) for key %s",
+			string(ts[0]),
+			t.Kind().String(),
+			k,
+		),
+	)
 }
+
+// func hashValue(o interface{}) []byte {
+// 	v := reflect.ValueOf(o)
+// 	t := reflect.TypeOf(o)
+// 	switch t.Kind() {
+// 	case reflect.Invalid: // nil
+// 		// return hash(HintNil, []byte{})
+// 		return nil
+// 	case reflect.Slice, reflect.Array:
+// 		if v.Len() == 0 {
+// 			return nil
+// 		}
+// 		if t.Elem() == reflect.TypeOf(byte(0)) {
+// 			return hash(HintData, o.([]byte))
+// 		}
+// 		vs := []interface{}{}
+// 		for i := 0; i < v.Len(); i++ {
+// 			vs = append(vs, v.Index(i).Interface())
+// 		}
+// 		b := []byte{}
+// 		for _, iv := range vs {
+// 			b = append(b, hashValue(iv)...)
+// 		}
+// 		return hash(HintArray, b)
+// 	case reflect.String:
+// 		if o.(string) == "" {
+// 			return nil
+// 		}
+// 		return hash(HintString, []byte(o.(string)))
+// 	case reflect.Struct:
+// 		panic("structs are not currently supported")
+// 	case reflect.Map:
+// 		m, ok := o.(map[string]interface{})
+// 		if !ok {
+// 			panic("hashing only supports map[string]interface{}")
+// 		}
+// 		if len(m) == 0 {
+// 			return nil
+// 		}
+// 		h, err := objecthash(m, false)
+// 		if err != nil {
+// 			panic("hashing error: " + err.Error())
+// 		}
+// 		return h
+// 	case reflect.Float32, reflect.Float64:
+// 		nf, err := hashFloat(v.Float())
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		return hash(HintFloat, []byte(nf))
+// 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+// 		return hash(HintInt, []byte(fmt.Sprintf("%d", v.Int())))
+// 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+// 		return hash(HintUint, []byte(fmt.Sprintf("%d", v.Uint())))
+// 	case reflect.Bool:
+// 		if v.Bool() {
+// 			return hash(HintBool, []byte{1})
+// 		}
+// 		return hash(HintBool, []byte{0})
+// 	}
+// 	panic("hash: unsupported type " + v.String() + " -- " + fmt.Sprintf("%#v", o))
+// }
 
 // replacing ben's implementation with something less custom, based on:
 // * https://github.com/benlaurie/objecthash
