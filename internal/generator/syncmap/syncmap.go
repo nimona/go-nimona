4615ec7f89ee28b7ab905439156f3a96af648977
George Antoniadis <george@noodles.gr>
2019-04-14 20:27:55 +0100

    feature(generator/syncmap): add generic syncmap

---
+++ internal/generator/syncmap/syncmap.go
@@ -0,0 +1,46 @@
+package syncmap
+
+import (
+	"sync"
+
+	"github.com/cheekybits/genny/generic"
+)
+
+type (
+	KeyType   generic.Type // nolint
+	ValueType generic.Type // nolint
+	// KeyTypeValueTypeSyncMap -
+	KeyTypeValueTypeSyncMap struct {
+		m sync.Map
+	}
+)
+
+// NewKeyTypeValueTypeSyncMap constructs a new SyncMap
+func NewKeyTypeValueTypeSyncMap() *KeyTypeValueTypeSyncMap {
+	return &KeyTypeValueTypeSyncMap{}
+}
+
+// Put -
+func (m *KeyTypeValueTypeSyncMap) Put(k KeyType, v *ValueType) {
+	m.m.Store(k, v)
+}
+
+// Get -
+func (m *KeyTypeValueTypeSyncMap) Get(k KeyType) (*ValueType, bool) {
+	i, ok := m.m.Load(k)
+	if !ok {
+		return nil, false
+	}
+
+	v, ok := i.(*ValueType)
+	if !ok {
+		return nil, false
+	}
+
+	return v, true
+}
+
+// Delete -
+func (m *KeyTypeValueTypeSyncMap) Delete(k KeyType) {
+	m.m.Delete(k)
+}
