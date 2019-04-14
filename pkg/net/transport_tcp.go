8c85364b874677d90cc1e2faf2e752189720d0cb
Paris Goudas <decimal@darkstar.gr>
2019-04-14 20:27:22 +0100

    Add tcp transport

---
+++ pkg/net/transport_tcp.go
@@ -0,0 +1,134 @@
+package net
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/tls"
+	"net"
+	"strings"
+	"time"
+
+	igd "github.com/emersion/go-upnp-igd"
+	"go.uber.org/zap"
+	"nimona.io/internal/log"
+	"nimona.io/pkg/crypto"
+)
+
+type tcpTransport struct {
+	local          *LocalInfo
+	relayAddresses []string
+}
+
+func NewTCPTransport(local *LocalInfo, relayAddresses []string) Transport {
+	return &tcpTransport{
+		local:          local,
+		relayAddresses: relayAddresses,
+	}
+}
+
+func (tt *tcpTransport) Dial(ctx context.Context, address string) (
+	*Connection, error) {
+
+	config := tls.Config{
+		InsecureSkipVerify: true,
+	}
+	addr := strings.Replace(address, "tcps:", "", 1)
+	dialer := net.Dialer{Timeout: time.Second}
+
+	tcpConn, err := tls.DialWithDialer(&dialer, "tcp", addr, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	if tcpConn == nil {
+		return nil, ErrAllAddressesFailed
+	}
+
+	conn := &Connection{
+		Conn:          tcpConn,
+		RemotePeerKey: nil, // we don't really know who the other side is
+	}
+
+	return conn, nil
+}
+
+func (tt *tcpTransport) Listen(ctx context.Context, address string) (
+	chan *Connection, error) {
+
+	logger := log.Logger(ctx).Named("network")
+	cert, err := crypto.GenerateCertificate(tt.local.GetPeerKey())
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+	config := tls.Config{
+		Certificates: []tls.Certificate{*cert},
+	}
+	config.NextProtos = []string{"nimona/1"} // TODO(geoah) is this of any actual use?
+	config.Time = func() time.Time { return now }
+	config.Rand = rand.Reader
+	tcpListener, err := tls.Listen("tcp", address, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	port := tcpListener.Addr().(*net.TCPAddr).Port
+	logger.Info("Listening and service nimona", zap.Int("port", port))
+	addresses := GetAddresses(tcpListener)
+	devices := make(chan igd.Device, 10)
+
+	if tt.local.GetHostname() != "" {
+		addresses = append(addresses, fmtAddress(tt.local.GetHostname(), port))
+	}
+
+	if UseUPNP {
+		logger.Info("Trying to find external IP and open port")
+		go func() {
+			if err := igd.Discover(devices, 2*time.Second); err != nil {
+				logger.Error("could not discover devices", zap.Error(err))
+			}
+		}()
+		for device := range devices {
+			externalAddress, err := device.GetExternalIPAddress()
+			if err != nil {
+				logger.Error("could not get external ip", zap.Error(err))
+				continue
+			}
+			desc := "nimona"
+			ttl := time.Hour * 24 * 365
+			if _, err := device.AddPortMapping(igd.TCP, port, port, desc, ttl); err != nil {
+				logger.Error("could not add port mapping", zap.Error(err))
+			} else {
+				addresses = append(addresses, fmtAddress(externalAddress.String(), port))
+			}
+		}
+	}
+
+	logger.Info("Started listening", zap.Strings("addresses", addresses))
+	tt.local.AddAddress(addresses...)
+	tt.local.AddAddress(tt.relayAddresses...)
+
+	cconn := make(chan *Connection, 10)
+	go func() {
+
+		for {
+			tcpConn, err := tcpListener.Accept()
+			if err != nil {
+				log.DefaultLogger.Warn(
+					"could not accept connection", zap.Error(err))
+				continue
+			}
+
+			conn := &Connection{
+				Conn:          tcpConn,
+				RemotePeerKey: nil,
+				IsIncoming:    true,
+			}
+
+			cconn <- conn
+		}
+	}()
+
+	return cconn, nil
+}
