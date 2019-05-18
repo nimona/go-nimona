0f80e38dc620a32d99adb236a76dd8d6a7a73222
George Antoniadis <george@noodles.gr>
2019-05-18 22:40:27 +0100

    chore(discovery): add subkey resolution test

--- pkg/discovery/hyperspace/resolver_test.go
+++ pkg/discovery/hyperspace/resolver_test.go
@@ -85,6 +85,45 @@ func TestDiscoverer_FindBothSides(t *testing.T) {
 	require.Equal(t, l1.GetPeerInfo().Addresses, peers[0].Addresses)
 }
 
+func TestDiscoverer_FindBothSides_SubKeys(t *testing.T) {
+	_, k0, n0, x0, disc0, l0, ctx0 := newPeer(t, "peer0")
+	ok1, k1, n1, x1, disc1, l1, ctx1 := newPeer(t, "peer1")
+	ok2, k2, n2, x2, disc2, l2, ctx2 := newPeer(t, "peer2")
+
+	fmt.Println("k0", k0.Fingerprint())
+	fmt.Println("k1", k1.Fingerprint())
+	fmt.Println("k2", k2.Fingerprint())
+
+	d0, err := NewDiscoverer(ctx0, n0, x0, l0, []string{})
+	assert.NoError(t, err)
+	err = disc0.AddProvider(d0)
+	assert.NoError(t, err)
+
+	ba := l0.GetPeerInfo().Addresses
+
+	d1, err := NewDiscoverer(ctx1, n1, x1, l1, ba)
+	assert.NoError(t, err)
+	err = disc1.AddProvider(d1)
+	assert.NoError(t, err)
+
+	d2, err := NewDiscoverer(ctx2, n2, x2, l2, ba)
+	assert.NoError(t, err)
+	err = disc2.AddProvider(d2)
+	assert.NoError(t, err)
+
+	ctxR1 := context.New(context.WithCorrelationID("req1"))
+	peers, err := d1.FindByFingerprint(ctxR1, ok2.Fingerprint())
+	require.NoError(t, err)
+	require.Len(t, peers, 1)
+	require.Equal(t, l2.GetPeerInfo().Addresses, peers[0].Addresses)
+
+	ctxR2 := context.New(context.WithCorrelationID("req2"))
+	peers, err = d2.FindByFingerprint(ctxR2, ok1.Fingerprint())
+	require.NoError(t, err)
+	require.Len(t, peers, 1)
+	require.Equal(t, l1.GetPeerInfo().Addresses, peers[0].Addresses)
+}
+
 func newPeer(
 	t *testing.T,
 	name string,
