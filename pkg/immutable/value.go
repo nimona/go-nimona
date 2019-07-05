129e3553de5db90649d2ccc1b4b6d8131913cab9
George Antoniadis <george@noodles.gr>
2019-07-05 19:20:32 +0100

    feat(immutable): fix lists, add anyToValue

--- pkg/immutable/value.go
+++ pkg/immutable/value.go
@@ -1,5 +1,12 @@
 package immutable
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
 type (
 	typeHinted interface {
 		typeHint() string
@@ -14,7 +21,10 @@ type (
 	floatValue  struct{ value float64 }
 	bytesValue  struct{ value []byte }
 	mapValue    struct{ value Map }
-	// listValue   struct{ value List }
+	listValue   struct {
+		hint  string
+		value List
+	}
 )
 
 func (v Value) BoolValue() bool {
@@ -58,12 +68,12 @@ func (v Value) MapValue() Map {
 	return Map{}
 }
 
-// func (v Value) ListValue() List {
-// 	if x, ok := v.kind.(listValue); ok {
-// 		return x.value
-// 	}
-// 	return List{}
-// }
+func (v Value) ListValue() List {
+	if x, ok := v.kind.(listValue); ok {
+		return x.value
+	}
+	return List{}
+}
 
 func (v Value) Primitive() interface{} {
 	switch b := v.kind.(type) {
@@ -78,9 +88,29 @@ func (v Value) Primitive() interface{} {
 	case bytesValue:
 		return v.kind.primitive()
 	case mapValue:
-		return b.value.Primitive()
-		// case listValue:
-		// 	return v.kind.primitive()
+		return b.value.primitive()
+	case listValue:
+		return b.value.primitive()
+	}
+	return nil
+}
+
+func (v Value) raw() interface{} {
+	switch v.kind.(type) {
+	case boolValue:
+		return v.BoolValue()
+	case stringValue:
+		return v.StringValue()
+	case intValue:
+		return v.IntValue()
+	case floatValue:
+		return v.FloatValue()
+	case bytesValue:
+		return v.BytesValue()
+	case mapValue:
+		return v.MapValue()
+	case listValue:
+		return v.ListValue()
 	}
 	return nil
 }
@@ -98,9 +128,9 @@ func (v Value) PrimitiveHinted() interface{} {
 	case bytesValue:
 		return v.kind.primitive()
 	case mapValue:
-		return b.value.PrimitiveHinted()
-		// case listValue:
-		// 	return v.kind.primitive()
+		return b.value.primitiveHinted()
+	case listValue:
+		return b.value.primitive()
 	}
 	return nil
 }
@@ -121,8 +151,7 @@ func (v intValue) typeHint() string    { return intTypeHint }
 func (v floatValue) typeHint() string  { return floatTypeHint }
 func (v bytesValue) typeHint() string  { return bytesTypeHint }
 func (v mapValue) typeHint() string    { return mapTypeHint }
-
-// func (v listValue) typeHint() string   { return listTypeHint }
+func (v listValue) typeHint() string   { return v.hint }
 
 func (v boolValue) primitive() interface{}   { return v.value }
 func (v stringValue) primitive() interface{} { return v.value }
@@ -130,5 +159,107 @@ func (v intValue) primitive() interface{}    { return v.value }
 func (v floatValue) primitive() interface{}  { return v.value }
 func (v bytesValue) primitive() interface{}  { return v.value }
 func (v mapValue) primitive() interface{}    { return v.value }
+func (v listValue) primitive() interface{}   { return v.value }
 
-// func (v listValue) primitive() interface{}   { return v.value }
+func getHints(k string) []string {
+	ps := strings.Split(k, ":")
+	if len(ps) == 1 {
+		return nil
+	}
+	hs := []string{}
+	for _, sh := range ps[1] {
+		hs = append(hs, string(sh))
+	}
+	return hs
+}
+
+func rmHints(k string) string {
+	ps := strings.Split(k, ":")
+	if len(ps) == 1 {
+		return k
+	}
+	return ps[0]
+}
+
+func AnyToValue(k string, a interface{}) Value {
+	hs := getHints(k)
+	fmt.Println(k, a, hs)
+
+	switch hs[0] {
+	case boolTypeHint:
+		switch v := a.(type) {
+		case bool:
+			return Value{
+				boolValue{v},
+			}
+		}
+
+	case stringTypeHint:
+		switch v := a.(type) {
+		case string:
+			return Value{
+				stringValue{v},
+			}
+		}
+
+	case intTypeHint:
+		switch v := a.(type) {
+		case int:
+			return Value{
+				intValue{int64(v)},
+			}
+		}
+
+	case floatTypeHint:
+		switch v := a.(type) {
+		case float32:
+			return Value{
+				floatValue{float64(v)},
+			}
+		}
+
+	case bytesTypeHint:
+		switch v := a.(type) {
+		case []byte:
+			return Value{
+				bytesValue{v},
+			}
+		}
+
+	case mapTypeHint:
+		switch v := a.(type) {
+		case map[interface{}]interface{}:
+			m := Map{}
+			for k, v := range v {
+				s, ok := k.(string)
+				if !ok {
+					panic("only string keys are allowed")
+				}
+				// TODO should we not be checking the hint?
+				m = m.Set(rmHints(s), AnyToValue(s, v))
+			}
+			return Value{
+				mapValue{m},
+			}
+		}
+
+	case listTypeHint:
+		switch v := a.(type) {
+		case []interface{}:
+			m := List{}
+			h := fmt.Sprintf(":%s", strings.Join(hs[1:], ""))
+			for _, v := range v {
+				m = m.Append(AnyToValue(h, v))
+			}
+			return Value{
+				listValue{
+					hint:  strings.Join(hs, ""),
+					value: m,
+				},
+			}
+		}
+	}
+
+	spew.Dump(k, a)
+	panic("not sure how to handle")
+}
