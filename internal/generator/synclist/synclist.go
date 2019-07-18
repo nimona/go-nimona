d0405549f6075493f11346fc790e147b56624193
George Antoniadis <george@noodles.gr>
2019-07-18 17:16:51 +0100

    feat(generator/synclist): add synclistt template

---
+++ internal/generator/synclist/synclist.go
@@ -0,0 +1,47 @@
+package syncmap
+
+import (
+	"sync"
+
+	"github.com/cheekybits/genny/generic"
+)
+
+type (
+	KeyType generic.Type // nolint
+	// KeyTypeSyncList -
+	KeyTypeSyncList struct {
+		m sync.Map
+	}
+)
+
+// NewKeyTypeValueTypeSyncMap constructs a new SyncMap
+func NewKeyTypeValueTypeSyncMap() *KeyTypeSyncList {
+	return &KeyTypeSyncList{}
+}
+
+// Put -
+func (m *KeyTypeSyncList) Put(k KeyType) {
+	m.m.Store(k, true)
+}
+
+// Exists -
+func (m *KeyTypeSyncList) Exists(k KeyType) bool {
+	_, ok := m.m.Load(k)
+	if !ok {
+		return false
+	}
+
+	return true
+}
+
+// Delete -
+func (m *KeyTypeSyncList) Delete(k KeyType) {
+	m.m.Delete(k)
+}
+
+// Range -
+func (m *KeyTypeSyncList) Range(i func(k KeyType) bool) {
+	m.m.Range(func(k, v interface{}) bool {
+		return i(k.(KeyType))
+	})
+}
