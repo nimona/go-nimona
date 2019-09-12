79d38ed2910460946c0e51fe67ef9723d1b68077
George Antoniadis <george@noodles.gr>
2019-09-12 20:17:21 +0100

    feat(log): add json log writer

---
+++ internal/log/writer_string.go
@@ -0,0 +1,44 @@
+package log
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"nimona.io/internal/context"
+)
+
+func StringWriter() Writer {
+	return func(log *logger, level Level, msg string, extraFields ...Field) {
+		ctx := log.getContext()
+		fields := log.getFields()
+		fields = append(fields, extraFields...)
+
+		res := map[string]interface{}{}
+		cID := context.GetCorrelationID(ctx)
+		if cID == "" {
+			cID = "-"
+		}
+
+		for _, field := range fields {
+			k := field.Key
+			v := field.Value
+			if s, ok := v.(interface{ String() string }); ok {
+				v = s.String()
+			} else if s, ok := v.(interface{ Error() string }); ok {
+				v = s.Error()
+			}
+			if _, ok := res[k]; !ok {
+				res[k] = v
+			}
+		}
+
+		j, _ := json.Marshal(res)
+		fmt.Printf(
+			"ctx=%s level=%s message=%s fields=%s\n",
+			cID,
+			levels[level],
+			msg,
+			string(j),
+		)
+	}
+}
