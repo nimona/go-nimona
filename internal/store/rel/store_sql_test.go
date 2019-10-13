1002c58a8458960a721d86026ed5a7d21edc3a3e
Paris Goudas <decimal@darkstar.gr>
2019-10-13 19:15:31 +0100

    feat(sql-store): make sql store accept db

--- internal/store/rel/store_sql_test.go
+++ internal/store/rel/store_sql_test.go
@@ -1,14 +1,21 @@
 package rel_test
 
 import (
+	"database/sql"
 	"testing"
 
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/require"
 	"nimona.io/internal/store/rel"
 )
 
+const dbFilepath string = "./nimona.db"
+
 func TestNewDatabase(t *testing.T) {
-	db, err := rel.New()
+	dblite, err := sql.Open("sqlite3", dbFilepath)
+	require.NoError(t, err)
+
+	db, err := rel.New(dblite)
 	require.NoError(t, err)
 	require.NotNil(t, db)
 
