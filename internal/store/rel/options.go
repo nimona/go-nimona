11bb45fa453229ca691513a7eb0bb416a7f5e37c
Paris Goudas <decimal@darkstar.gr>
2019-10-15 23:42:43 +0100

    feat(sql-store): add options in store

---
+++ internal/store/rel/options.go
@@ -0,0 +1,14 @@
+package rel
+
+type (
+	Options struct {
+		TTL int // minutes
+	}
+	Option func(*Options)
+)
+
+func WithTTL(minutes int) Option {
+	return func(o *Options) {
+		o.TTL = minutes
+	}
+}
