5062ee59dd437d59cda9dcb341cbf5c4f144fdf0
George Antoniadis <george@noodles.gr>
2019-11-19 19:27:09 +0000

    feat(pubsub): add queue based pubsub

---
+++ internal/pubsub/pubsub_test.go
@@ -0,0 +1,85 @@
+package pubsub
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPubSubSimple(t *testing.T) {
+	ps := New()
+	ps.Publish("one")
+
+	s := ps.Subscribe()
+	ps.Publish("two")
+	ps.Publish("three")
+
+	next, err := s.Next()
+	require.Equal(t, "two", next)
+	require.NoError(t, err)
+
+	next, err = s.Next()
+	require.Equal(t, "three", next)
+	require.NoError(t, err)
+}
+
+func TestPubSubFiltered(t *testing.T) {
+	ps := New()
+
+	f1 := func(v interface{}) bool {
+		return strings.HasPrefix(v.(string), "t")
+	}
+
+	s := ps.Subscribe(f1)
+	ps.Publish("one")
+	ps.Publish("two")
+	ps.Publish("three")
+
+	next, err := s.Next()
+	require.Equal(t, "two", next)
+	require.NoError(t, err)
+
+	next, err = s.Next()
+	require.Equal(t, "three", next)
+	require.NoError(t, err)
+}
+
+func TestPubSubFilteredMultiple(t *testing.T) {
+	ps := New()
+
+	f1 := func(v interface{}) bool {
+		return strings.HasPrefix(v.(string), "t")
+	}
+
+	f2 := func(v interface{}) bool {
+		return strings.HasSuffix(v.(string), "e")
+	}
+
+	s := ps.Subscribe(f1, f2)
+	ps.Publish("one")
+	ps.Publish("two")
+	ps.Publish("three")
+
+	next, err := s.Next()
+	require.Equal(t, "three", next)
+	require.NoError(t, err)
+}
+
+func TestPubSubCancel(t *testing.T) {
+	ps := New()
+
+	s := ps.Subscribe()
+	ps.Publish("one")
+	ps.Publish("two")
+
+	next, err := s.Next()
+	require.Equal(t, "one", next)
+	require.NoError(t, err)
+
+	s.Cancel()
+
+	next, err = s.Next()
+	require.Equal(t, nil, next)
+	require.Error(t, err, ErrSubscriptionCanceled)
+}
