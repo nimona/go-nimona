a0f9f781a480637c204c2b83a8d4d9298b1cfdda
George Antoniadis <george@noodles.gr>
2019-11-27 00:15:41 +0000

    feat(store/sql): add ContentType to Objects table

--- internal/store/sql/store_migrations.go
+++ internal/store/sql/store_migrations.go
@@ -3,6 +3,7 @@ package sql
 var migrations = [...]string{
 	`CREATE TABLE IF NOT EXISTS Objects (
 	  Hash TEXT NOT NULL PRIMARY KEY,
+	  Type TEXT,
 	  StreamHash TEXT,
 	  Body BLOB,
 	  Created INT,
