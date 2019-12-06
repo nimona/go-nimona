80052476414ef61e2451a93822a71d996b61584b
George Antoniadis <george@noodles.gr>
2019-12-06 17:14:24 +0000

    feat(store/sql): add filter method with functional filters same as the ones in subscribe

--- internal/store/sql/pubsub_filters.go
+++ internal/store/sql/pubsub_filters.go
@@ -3,24 +3,52 @@ package sql
 import (
 	"github.com/gobwas/glob"
 	"nimona.io/pkg/errors"
+	"nimona.io/pkg/hash"
 	"nimona.io/pkg/object"
 )
 
-func FilterByStreamHash(h object.Hash) SqlStoreFilter {
-	return func(o object.Object) bool {
-		os := o.Get("stream:s")
-		switch oh := os.(type) {
-		case object.Hash:
-			return h.IsEqual(oh)
-		case string:
-			return h.String() == os
-		default:
-			return false
+// LookupOptions
+type (
+	LookupOption  func(*LookupOptions)
+	LookupOptions struct {
+		// Lookups are used to perform db queries for these filters
+		Lookups struct {
+			ObjectHashes []object.Hash
+			StreamHashes []object.Hash
+			ContentTypes []string
 		}
+		// filters are the lookups equivalents for matching objects for pubsub
+		Filters []SqlStoreFilter
+	}
+)
+
+func FilterByHash(h object.Hash) LookupOption {
+	return func(opts *LookupOptions) {
+		opts.Lookups.ObjectHashes = append(opts.Lookups.ObjectHashes, h)
+		opts.Filters = append(opts.Filters, func(o object.Object) bool {
+			return hash.New(o) == h
+		})
 	}
 }
 
-func FilterByObjectType(typePatterns ...string) SqlStoreFilter {
+func FilterByStreamHash(h object.Hash) LookupOption {
+	return func(opts *LookupOptions) {
+		opts.Lookups.StreamHashes = append(opts.Lookups.StreamHashes, h)
+		opts.Filters = append(opts.Filters, func(o object.Object) bool {
+			os := o.Get("stream:s")
+			switch oh := os.(type) {
+			case object.Hash:
+				return h.IsEqual(oh)
+			case string:
+				return h.String() == os
+			default:
+				return false
+			}
+		})
+	}
+}
+
+func FilterByObjectType(typePatterns ...string) LookupOption {
 	patterns := make([]glob.Glob, len(typePatterns))
 	for i, typePattern := range typePatterns {
 		g, err := glob.Compile(typePattern, '.', '/', '#')
@@ -29,12 +57,15 @@ func FilterByObjectType(typePatterns ...string) SqlStoreFilter {
 		}
 		patterns[i] = g
 	}
-	return func(o object.Object) bool {
-		for _, pattern := range patterns {
-			if pattern.Match(o.GetType()) {
-				return true
+	return func(opts *LookupOptions) {
+		opts.Lookups.ContentTypes = append(opts.Lookups.ContentTypes, typePatterns...)
+		opts.Filters = append(opts.Filters, func(o object.Object) bool {
+			for _, pattern := range patterns {
+				if pattern.Match(o.GetType()) {
+					return true
+				}
 			}
-		}
-		return false
+			return false
+		})
 	}
 }
