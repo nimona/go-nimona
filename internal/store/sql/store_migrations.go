c247b3f3a2ef2f8e55a7cf091b9a27d3195c09cb
George Antoniadis <george@noodles.gr>
2019-12-07 00:05:33 +0000

    refactor(store/sql): move to smaller migrations

--- internal/store/sql/store_migrations.go
+++ internal/store/sql/store_migrations.go
@@ -1,14 +1,11 @@
 package sql
 
 var migrations = [...]string{
-	`CREATE TABLE IF NOT EXISTS Objects (
-	  Hash TEXT NOT NULL PRIMARY KEY,
-	  Type TEXT,
-	  StreamHash TEXT,
-	  Body BLOB,
-	  Created INT,
-	  LastAccessed INT,
-	  TTL INT
-	 )
-	`,
+	`CREATE TABLE IF NOT EXISTS Objects (Hash TEXT NOT NULL PRIMARY KEY);`,
+	`ALTER TABLE Objects ADD Type TEXT;`,
+	`ALTER TABLE Objects ADD Body BLOB;`,
+	`ALTER TABLE Objects ADD RootHash TEXT;`,
+	`ALTER TABLE Objects ADD TTL INT;`,
+	`ALTER TABLE Objects ADD Created INT;`,
+	`ALTER TABLE Objects ADD LastAccessed INT;`,
 }
