e370f99165c89aa704f9bbb0f05dd45683261a80
George Antoniadis <george@noodles.gr>
2019-12-08 21:18:07 +0000

    refactor(store/sql): change body into a text

--- internal/store/sql/store_migrations.go
+++ internal/store/sql/store_migrations.go
@@ -3,7 +3,7 @@ package sql
 var migrations = [...]string{
 	`CREATE TABLE IF NOT EXISTS Objects (Hash TEXT NOT NULL PRIMARY KEY);`,
 	`ALTER TABLE Objects ADD Type TEXT;`,
-	`ALTER TABLE Objects ADD Body BLOB;`,
+	`ALTER TABLE Objects ADD Body TEXT;`,
 	`ALTER TABLE Objects ADD RootHash TEXT;`,
 	`ALTER TABLE Objects ADD TTL INT;`,
 	`ALTER TABLE Objects ADD Created INT;`,
