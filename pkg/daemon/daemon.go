bcf20b67109fd74e46b908fb88648adeaf173d5c
Paris Goudas <decimal@darkstar.gr>
2020-04-10 00:19:25 +0100

    feat(connmanager): use connmanager in daemon

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -8,6 +8,7 @@ import (
 	// required for sqlobjectstore
 	_ "github.com/mattn/go-sqlite3"
 
+	"nimona.io/pkg/connmanager"
 	"nimona.io/pkg/context"
 	"nimona.io/pkg/crypto"
 	"nimona.io/pkg/daemon/config"
@@ -93,6 +94,11 @@ func New(ctx context.Context, cfg *config.Config) (*Daemon, error) {
 	// add middleware to network
 	network.AddMiddleware(handshakeMiddleware.Handle())
 
+	cmgr, err := connmanager.New(ctx, network, li)
+	if err != nil {
+		return nil, errors.Wrap(errors.New("could not construct connection manager"), err)
+	}
+
 	// construct exchange
 	ex, err := exchange.New(
 		ctx,
@@ -101,6 +107,7 @@ func New(ctx context.Context, cfg *config.Config) (*Daemon, error) {
 		st,
 		ps,
 		li,
+		cmgr,
 	)
 	if err != nil {
 		return nil, errors.Wrap(errors.New("could not construct exchange"), err)
