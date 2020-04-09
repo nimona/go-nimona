69d355a0b7ebd949c367a2823fdd019d5b04a215
Paris Goudas <decimal@darkstar.gr>
2020-04-09 23:29:25 +0100

    feat(connmanager): add connection manager

---
+++ pkg/connmanager/connmanager.go
@@ -0,0 +1,138 @@
+package connmanager
+
+import (
+	"errors"
+	"sync"
+
+	"nimona.io/internal/generator/queue"
+	"nimona.io/pkg/context"
+	"nimona.io/pkg/crypto"
+	"nimona.io/pkg/net"
+	"nimona.io/pkg/peer"
+)
+
+//go:generate $GOBIN/genny -in=$GENERATORS/syncmap_named/syncmap.go -out=addresses_generated.go -pkg=connmanager gen "KeyType=string ValueType=addressState SyncmapName=addresses"
+//go:generate $GOBIN/genny -in=$GENERATORS/syncmap_named/syncmap.go -out=connections_generated.go -imp=nimona.io/pkg/crypto -pkg=connmanager gen "KeyType=crypto.PublicKey ValueType=peerbox SyncmapName=connections"
+
+type addressState int
+
+type peerbox struct {
+	peer      crypto.PublicKey
+	addresses *AddressesMap
+	conn      *net.Connection
+	connLock  sync.RWMutex
+	queue     *queue.Queue
+}
+
+type Manager interface {
+	GetConnection(context.Context, *peer.Peer) (*net.Connection, error)
+}
+
+type manager struct {
+	net net.Network
+
+	// store the connections per peer
+	connections *ConnectionsMap
+	local       *peer.LocalPeer
+}
+
+func New(
+	ctx context.Context,
+	n net.Network,
+	localInfo *peer.LocalPeer,
+) (Manager, error) {
+
+	mgr := &manager{
+		net:         n,
+		connections: NewConnectionsMap(),
+		local:       localInfo,
+	}
+
+	incomingConnections, err := n.Listen(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// handle new incoming connections
+	go func() {
+		for {
+			conn := <-incomingConnections
+			go func(conn *net.Connection) {
+				if err := mgr.handleConnection(conn); err != nil {
+					// TODO
+					// logger.Warn("failed to handle connection", log.Error(err))
+				}
+			}(conn)
+		}
+	}()
+
+	return mgr, nil
+}
+
+func (m *manager) GetConnection(
+	ctx context.Context,
+	peer *peer.Peer,
+) (*net.Connection, error) {
+
+	pbox := m.getPeerbox(peer.PublicKey())
+
+	pbox.connLock.RLock()
+	if pbox.conn != nil {
+		pbox.connLock.RUnlock()
+		return pbox.conn, nil
+	}
+
+	pbox.connLock.RUnlock()
+	conn, err := m.net.Dial(ctx, peer)
+	if err != nil {
+		// todo log
+		return nil, err
+	}
+
+	if err := m.handleConnection(conn); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func (m *manager) handleConnection(
+	conn *net.Connection,
+) error {
+	if conn == nil {
+		panic(errors.New("missing connection"))
+	}
+
+	// find existing peerbox and update it
+	pbox := m.getPeerbox(conn.RemotePeerKey)
+	m.updateConnection(pbox, conn)
+
+	if err := net.Write(
+		m.local.GetSignedPeer().ToObject(),
+		conn,
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *manager) updateConnection(pbox *peerbox, conn *net.Connection) {
+	pbox.connLock.Lock()
+	if pbox.conn != nil {
+		pbox.conn.Close() // nolint: errcheck
+	}
+	pbox.conn = conn
+	pbox.connLock.Unlock()
+}
+
+func (m *manager) getPeerbox(peer crypto.PublicKey) *peerbox {
+	pbx := &peerbox{
+		peer:      peer,
+		addresses: NewAddressesMap(),
+	}
+
+	pbx, _ = m.connections.GetOrPut(peer, pbx)
+
+	return pbx
+}
