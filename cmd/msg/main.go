1193c7bb4cbc7b830dafe97a7d6610b5c8c679a1
George Antoniadis <george@noodles.gr>
2020-05-22 15:37:23 +0100

    fix(cmd/msg): update for v0.8.0 API

--- cmd/msg/main.go
+++ cmd/msg/main.go
@@ -9,12 +9,14 @@ import (
 
 	"nimona.io/pkg/context"
 	"nimona.io/pkg/crypto"
-	"nimona.io/pkg/daemon"
 	"nimona.io/pkg/daemon/config"
+	"nimona.io/pkg/eventbus"
 	"nimona.io/pkg/exchange"
-	"nimona.io/pkg/log"
+	"nimona.io/pkg/keychain"
+	"nimona.io/pkg/nat"
+	"nimona.io/pkg/net"
 	"nimona.io/pkg/peer"
-	"nimona.io/pkg/version"
+	"nimona.io/pkg/resolver"
 )
 
 func main() {
@@ -22,60 +24,114 @@ func main() {
 		context.WithCorrelationID("nimona"),
 	)
 
-	nodeAlias := os.Getenv("NIMONA_ALIAS")
-	if nodeAlias != "" {
-		log.DefaultLogger = log.DefaultLogger.With(
-			log.String("$alias", nodeAlias),
-		)
-	}
-
-	logger := log.FromContext(ctx).With(
-		log.String("build.version", version.Version),
-		log.String("build.commit", version.Commit),
-		log.String("build.timestamp", version.Date),
-	)
-
 	// load config
-	logger.Info("loading config file")
-	config := config.New()
-	if err := config.Load(); err != nil {
-		logger.Fatal("could not load config file", log.Error(err))
+	fmt.Println("* loading config file")
+	cfg := config.New()
+	if err := cfg.Load(); err != nil {
+		fmt.Println("could not load config file, error:", err)
+		os.Exit(1)
 	}
 
 	// create peer key pair if it does not exist
-	if config.Peer.PeerKey == "" {
-		logger.Info("creating new peer key pair")
+	if cfg.Peer.PeerKey == "" {
+		fmt.Println("* creating new peer key pair")
 		peerKey, err := crypto.GenerateEd25519PrivateKey()
 		if err != nil {
-			logger.Fatal("could not generate peer key", log.Error(err))
+			fmt.Println("could not generate peer key, error:", err)
+			os.Exit(1)
 		}
-		config.Peer.PeerKey = peerKey
+		cfg.Peer.PeerKey = peerKey
 	}
 
 	// create identity key pair if it does not exist
 	// TODO this is temporary
-	if config.Peer.IdentityKey == "" {
-		logger.Info("creating new identity key pair")
+	if cfg.Peer.IdentityKey == "" {
+		fmt.Println("* creating new identity key pair")
 		identityKey, err := crypto.GenerateEd25519PrivateKey()
 		if err != nil {
-			logger.Fatal("could not generate identity key", log.Error(err))
+			fmt.Println("could not generate identity key, error:", err)
+			os.Exit(1)
 		}
-		config.Peer.IdentityKey = identityKey
+		cfg.Peer.IdentityKey = identityKey
 	}
 
 	// update config
-	if err := config.Update(); err != nil {
-		logger.Fatal("could not update config", log.Error(err))
+	if err := cfg.Update(); err != nil {
+		fmt.Println("could not update config, error:", err)
+		os.Exit(1)
 	}
 
-	logger.Info("loaded config", log.Any("config", config))
+	// add identity key to local info
+	if cfg.Peer.IdentityKey != "" {
+		keychain.DefaultKeychain.Put(
+			keychain.IdentityKey,
+			cfg.Peer.IdentityKey,
+		)
+	}
 
-	d, err := daemon.New(ctx, config)
+	// add relay peers
+	for i, rp := range cfg.Peer.RelayKeys {
+		eventbus.DefaultEventbus.Publish(
+			eventbus.RelayAdded{
+				Peer: &peer.Peer{
+					Owners: []crypto.PublicKey{
+						crypto.PublicKey(rp),
+					},
+					Addresses: []string{
+						cfg.Peer.RelayAddresses[i],
+					},
+				},
+			},
+		)
+	}
+
+	keychain.DefaultKeychain.Put(
+		keychain.PrimaryPeerKey,
+		cfg.Peer.PeerKey,
+	)
+
+	// get temp bootstrap peers from cfg
+	bootstrapPeers := make([]*peer.Peer, len(cfg.Peer.BootstrapKeys))
+	for i, k := range cfg.Peer.BootstrapKeys {
+		bootstrapPeers[i] = &peer.Peer{
+			Owners: []crypto.PublicKey{
+				crypto.PublicKey(k),
+			},
+		}
+	}
+	for i, a := range cfg.Peer.BootstrapAddresses {
+		bootstrapPeers[i].Addresses = []string{a}
+	}
+
+	fmt.Println("* connecting to the network")
+
+	if _, err := net.DefaultNetwork.Listen(
+		ctx,
+		fmt.Sprintf("0.0.0.0:%d", cfg.Peer.TCPPort),
+	); err != nil {
+		fmt.Println("could not start listening, error:", err)
+		os.Exit(1)
+	}
+
+	if err := resolver.DefaultResolver.Bootstrap(
+		context.New(
+			context.WithTimeout(time.Second*5),
+		),
+		bootstrapPeers...,
+	); err != nil {
+		fmt.Println("could not bootstrap, error:", err)
+		os.Exit(1)
+	}
+
+	rmBinding, err := nat.MapExternalPort(cfg.Peer.TCPPort)
 	if err != nil {
-		logger.Fatal("could not construct daemon", log.Error(err))
+		fmt.Println("* could not create UPNP mapping, error:", err)
 	}
+	defer rmBinding()
 
-	sub := d.Exchange.Subscribe(exchange.FilterByObjectType("nimona.io/msg"))
+	sub := exchange.DefaultExchange.Subscribe(
+		exchange.FilterByObjectType("nimona.io/msg"),
+	)
 	go func() {
 		for {
 			e, err := sub.Next()
@@ -86,37 +142,71 @@ func main() {
 		}
 	}()
 
+	fmt.Println("* ready")
+	fmt.Println("* type `info` to see your peer's information.")
+	fmt.Println("* type `send <recipient's key> <message>` to send a message.")
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		text := scanner.Text()
-		switch text {
-		case "/info":
-			fmt.Println("addresses", d.LocalPeer.GetAddresses())
-			fmt.Println("peer", config.Peer.PeerKey.PublicKey().String())
-			fmt.Println("identity", config.Peer.IdentityKey.PublicKey().String())
-			continue
-		}
-		ps := strings.Split(text, " ")
-		if len(ps) < 2 {
-			fmt.Println("missing recipient")
-			continue
-		}
-		recipient := ps[0]
-		body := strings.Join(ps[1:], " ")
-		msg := Msg{
-			Datetime: time.Now().Unix(),
-			Body:     body,
-		}
-		err := d.Exchange.Send(
-			context.New(),
-			msg.ToObject(),
-			peer.LookupByOwner(crypto.PublicKey(recipient)),
-			exchange.WithAsync(),
-		)
-		go func() {
-			if err != nil {
-				fmt.Println("* could not send message;", err)
+		cmd := strings.Split(text, " ")[0]
+		switch cmd {
+		case "info":
+			fmt.Println("> addresses", net.DefaultNetwork.Addresses())
+			fmt.Println("> peer", cfg.Peer.PeerKey.PublicKey().String())
+			fmt.Println("> identity", cfg.Peer.IdentityKey.PublicKey().String())
+		case "exit", "quit":
+			fmt.Println("> goodbye 👋")
+			return
+		case "send":
+			ps := strings.Split(text, " ")
+			if len(ps) < 3 {
+				fmt.Println("missing recipient")
+				continue
 			}
-		}()
+			recipient := ps[1]
+			fmt.Println("* looking for ", recipient)
+			body := strings.Join(ps[2:], " ")
+			msg := Msg{
+				Datetime: time.Now().Unix(),
+				Body:     body,
+			}
+			rs, err := resolver.DefaultResolver.Lookup(
+				context.New(
+					context.WithTimeout(time.Second),
+				),
+				resolver.LookupByOwner(crypto.PublicKey(recipient)),
+			)
+			if err != nil {
+				fmt.Println("* could not find recipient, error:, error:", err)
+				break
+			}
+			sent := 0
+			failed := 0
+			for r := range rs {
+				fmt.Println("** found on", r.Addresses)
+				if err := exchange.DefaultExchange.Send(
+					context.New(
+						context.WithTimeout(time.Second),
+					),
+					msg.ToObject(),
+					r,
+				); err != nil {
+					failed++
+					fmt.Println("* could not send message;, error:", err)
+					continue
+				}
+				sent++
+			}
+			if sent > 0 {
+				fmt.Println("* message sent")
+			} else if sent == 0 && failed == 0 {
+				fmt.Println("* could not find recipient")
+			} else {
+				fmt.Println("* could not send message")
+			}
+		default:
+			fmt.Println("* not sure how to handle", cmd)
+		}
 	}
 }
