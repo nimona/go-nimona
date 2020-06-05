e25939885cd3c5220e3e2d15a450ca143d2d32aa
George Antoniadis <george@noodles.gr>
2020-06-05 23:39:26 +0100

    feat(playground/certificate-request): add certificate-request example cli

---
+++ playground/certificate-request/main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/websocket"
+	"github.com/skip2/go-qrcode"
+
+	"nimona.io/internal/rand"
+	"nimona.io/pkg/exchange"
+	"nimona.io/pkg/keychain"
+	"nimona.io/pkg/peer"
+)
+
+var indexTemplate = `
+<div>nonce: {{ .Nonce }}</div>
+<div>asr: {{ .ASR }}</div>
+<div><img src="qr.png?s={{ .ASR }}" /></div>
+<!--
+<input id="input" type="text" />
+<button onclick="send()">Send</button>
+-->
+<pre id="output"></pre>
+<script>
+    var input = document.getElementById("input");
+    var output = document.getElementById("output");
+    var socket = new WebSocket("ws://nimona.io:8080/echo?n={{ .Nonce }}");
+
+    socket.onopen = function () {
+        output.innerHTML += "status: waiting\n";
+    };
+
+    socket.onmessage = function (e) {
+        output.innerHTML += "server: " + e.data + "\n";
+    };
+
+    function send() {
+        socket.send(input.value);
+        input.value = "";
+    }
+</script>
+`
+
+type Values struct {
+	Nonce string
+	ASR   string
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// nolint: errcheck
+func main() {
+	http.HandleFunc("/qr.png", func(w http.ResponseWriter, r *http.Request) {
+		s := strings.Replace(r.URL.Query().Get("s"), "/qr.png?s=", "", 1)
+		fmt.Println(">>>", s)
+		png, _ := qrcode.Encode(s, qrcode.Medium, 256)
+		w.Header().Set("content-type", "image/png")
+		w.Write(png)
+	})
+
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("*** sub start")
+		conn, _ := upgrader.Upgrade(w, r, nil)
+		sub := exchange.Subscribe(
+			exchange.FilterByObjectType("nimona.io/peer.Certificate"),
+		)
+		n := strings.Replace(r.URL.Query().Get("n"), "/echo?n=", "", 1)
+		fmt.Println("nonce:", n)
+
+		conn.WriteMessage(
+			websocket.TextMessage,
+			[]byte("status: subscribed to events"),
+		)
+		defer fmt.Println("*** sub stop")
+		defer sub.Cancel()
+		go func() {
+			for {
+				e, err := sub.Next()
+				if err != nil {
+					return
+				}
+				fmt.Println("*** GOT", e.Payload.GetType())
+				r := &peer.Certificate{}
+				if err := r.FromObject(e.Payload); err != nil {
+					conn.WriteMessage(
+						websocket.TextMessage,
+						[]byte(
+							"update: received invalid event from "+
+								e.Sender.String(),
+						),
+					)
+					continue
+				}
+
+				if r.Nonce != n {
+					conn.WriteMessage(
+						websocket.TextMessage,
+						[]byte(
+							"update: received invalid nonce from "+
+								e.Sender.String(),
+						),
+					)
+					continue
+				}
+
+				conn.WriteMessage(
+					websocket.TextMessage,
+					[]byte("update: signed by "+e.Sender.String()),
+				)
+
+				conn.WriteMessage(
+					websocket.TextMessage,
+					[]byte("status: done "),
+				)
+			}
+		}()
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+		}
+	})
+
+	serve()
+	tmpl := template.Must(template.New("index").Parse(indexTemplate))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n := rand.String(8)
+		asr := &peer.CertificateRequest{
+			ApplicationName:        "Foobar App",
+			ApplicationDescription: "An app that does nothing",
+			ApplicationURL:         "https://github.com/nimona",
+			Subject:                keychain.GetPrimaryPeerKey().PublicKey().String(),
+			Resources: []string{
+				"nimona.io/**",
+				"mochi.io/**",
+			},
+			Actions: []string{
+				"read",
+				"write",
+				"delete",
+				"archive",
+			},
+			Nonce: n,
+		}
+
+		m := asr.ToObject().ToMap()
+		delete(m, "_signatures:ao")
+
+		b, _ := json.Marshal(m)
+		s := string(b)
+		v := Values{
+			ASR:   "/qr.png?s=" + s,
+			Nonce: n,
+		}
+		w.Header().Set("content-type", "text/html")
+		tmpl.Execute(w, v) // nolint: errcheck
+	})
+
+	http.ListenAndServe(":8080", nil) // nolint: errcheck
+}
---
+++ playground/certificate-request/serve.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"nimona.io/pkg/context"
+	"nimona.io/pkg/crypto"
+	"nimona.io/pkg/daemon/config"
+	"nimona.io/pkg/eventbus"
+	"nimona.io/pkg/keychain"
+	"nimona.io/pkg/net"
+	"nimona.io/pkg/peer"
+	"nimona.io/pkg/resolver"
+)
+
+func serve() {
+	ctx := context.New()
+
+	// load config
+	fmt.Println("* loading config file")
+	cfg := config.New()
+	if err := cfg.Load(); err != nil {
+		fmt.Println("could not load config file, error:", err)
+		os.Exit(1)
+	}
+
+	// create peer key pair if it does not exist
+	if cfg.Peer.PeerKey == "" {
+		fmt.Println("* creating new peer key pair")
+		peerKey, err := crypto.GenerateEd25519PrivateKey()
+		if err != nil {
+			fmt.Println("could not generate peer key, error:", err)
+			os.Exit(1)
+		}
+		cfg.Peer.PeerKey = peerKey
+	}
+
+	// add identity key to local info
+	if cfg.Peer.IdentityKey != "" {
+		keychain.Put(
+			keychain.IdentityKey,
+			cfg.Peer.IdentityKey,
+		)
+	}
+
+	// add relay peers
+	for i, rp := range cfg.Peer.RelayKeys {
+		eventbus.Publish(
+			eventbus.RelayAdded{
+				Peer: &peer.Peer{
+					Owners: []crypto.PublicKey{
+						crypto.PublicKey(rp),
+					},
+					Addresses: []string{
+						cfg.Peer.RelayAddresses[i],
+					},
+				},
+			},
+		)
+	}
+
+	keychain.Put(
+		keychain.PrimaryPeerKey,
+		cfg.Peer.PeerKey,
+	)
+
+	// get temp bootstrap peers from cfg
+	bootstrapPeers := make([]*peer.Peer, len(cfg.Peer.BootstrapKeys))
+	for i, k := range cfg.Peer.BootstrapKeys {
+		bootstrapPeers[i] = &peer.Peer{
+			Owners: []crypto.PublicKey{
+				crypto.PublicKey(k),
+			},
+		}
+	}
+	for i, a := range cfg.Peer.BootstrapAddresses {
+		bootstrapPeers[i].Addresses = []string{a}
+	}
+
+	fmt.Println("* connecting to the network")
+
+	if _, err := net.Listen(
+		ctx,
+		fmt.Sprintf("0.0.0.0:%d", cfg.Peer.TCPPort),
+	); err != nil {
+		fmt.Println("could not start listening, error:", err)
+		os.Exit(1)
+	}
+
+	if err := resolver.Bootstrap(
+		context.New(
+			context.WithTimeout(time.Second*5),
+		),
+		bootstrapPeers...,
+	); err != nil {
+		fmt.Println("could not bootstrap, error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(
+		"* ready",
+		cfg.Peer.PeerKey,
+		cfg.Peer.PeerKey.PublicKey(),
+		net.Addresses(),
+	)
+}
