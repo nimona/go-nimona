47dfbc3e2d5fb168c1d1c71622ed2b65444cbd56
Paris Goudas <paris@darkstar.gr>
2020-06-17 21:13:16 +0100

    feat(blob): add FromBlob

--- pkg/blob/blob.go
+++ pkg/blob/blob.go
@@ -5,7 +5,18 @@ import (
 	"io"
 )
 
-const maxCapacity = 1024 * 1024
+const maxCapacity = 1000 * 1000
+
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
+
+type blobReader struct {
+	total      int
+	chunkIndex int
+	dataIndex  int
+	blob       *Blob
+}
 
 func ToBlob(r io.Reader) (*Blob, error) {
 	blob := Blob{}
@@ -14,7 +25,7 @@ func ToBlob(r io.Reader) (*Blob, error) {
 	br := bufio.NewReaderSize(r, maxCapacity)
 	for {
 		buf := make([]byte, maxCapacity)
-		_, err := br.Read(buf)
+		n, err := br.Read(buf)
 		if err == io.EOF {
 			break
 		}
@@ -23,7 +34,7 @@ func ToBlob(r io.Reader) (*Blob, error) {
 		}
 
 		ch := &Chunk{
-			Data: buf,
+			Data: buf[0:n],
 		}
 
 		chunks = append(chunks, ch)
@@ -32,4 +43,61 @@ func ToBlob(r io.Reader) (*Blob, error) {
 	blob.Chunks = chunks
 
 	return &blob, nil
+}
+func FromBlob(bl *Blob) Reader {
+	return &blobReader{
+		blob:      bl,
+		dataIndex: 0,
+	}
+}
+
+func (bl *blobReader) Read(p []byte) (n int, err error) {
+	maxBuffer := len(p)
+	dataRead := 0
+
+	// we need to use a temp buf to combine slices from multiple reads
+	tempBuf := make([]byte, 0)
+
+	// read until while the buffer is full
+	for dataRead < maxBuffer {
+		// check if we have already read all the chunks
+		if bl.chunkIndex >= len(bl.blob.Chunks) {
+			break
+		}
+
+		// check the remaining data on the current chunk
+		remainingData := len(bl.blob.Chunks[bl.chunkIndex].Data[bl.dataIndex:])
+		diff := remainingData - maxBuffer
+
+		// find the limits for the current read
+		lower := bl.dataIndex
+		upper := maxBuffer + bl.dataIndex - dataRead
+
+		if diff < 0 {
+			upper = len(bl.blob.Chunks[bl.chunkIndex].Data)
+		}
+
+		// append to the temp buf
+		tempBuf = append(tempBuf, bl.blob.Chunks[bl.chunkIndex].Data[lower:upper]...)
+		dataRead += upper - lower
+
+		// adjust the indexes
+		bl.dataIndex += upper - lower
+
+		if diff < 0 {
+			bl.dataIndex = 0
+			bl.chunkIndex++
+		}
+	}
+
+	bl.total += dataRead
+
+	// finally copy to the right byte array
+	if len(tempBuf) == 0 {
+		return 0, io.EOF
+	}
+
+	copy(p, tempBuf)
+
+	return dataRead, nil
 }
