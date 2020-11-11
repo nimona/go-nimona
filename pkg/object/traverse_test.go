0330a5cc4b7db97d8847774238752f3abcf5e24f
George Antoniadis <george@noodles.gr>
2020-11-11 17:11:02 +0000

    chore(object): add traverse tests

--- pkg/object/traverse_test.go
+++ pkg/object/traverse_test.go
@@ -1,152 +1,127 @@
 package object
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/stretchr/testify/assert"
+)
 
-// func Test_Traverse(t *testing.T) {
-// 	type args struct {
-// 		v Value
-// 	}
-// 	tests := []struct {
-// 		name      string
-// 		args      args
-// 		stopAfter int
-// 		want      map[string]Value
-// 	}{{
-// 		name: "should pass, simple value",
-// 		args: args{
-// 			Int(1),
-// 		},
-// 		want: map[string]Value{
-// 			"": Int(1),
-// 		},
-// 	}, {
-// 		name: "should pass, simple list",
-// 		args: args{
-// 			v: List{}.
-// 				Append(Int(0)).
-// 				Append(Int(1)).
-// 				Append(Int(2)),
-// 		},
-// 		want: map[string]Value{
-// 			"": List{}.
-// 				Append(Int(0)).
-// 				Append(Int(1)).
-// 				Append(Int(2)),
-// 			"0": Int(0),
-// 			"1": Int(1),
-// 			"2": Int(2),
-// 		},
-// 	}, {
-// 		name: "should pass, simple map",
-// 		args: args{
-// 			v: Map{}.
-// 				Set("foo0:s", String("bar0")).
-// 				Set("foo1:s", String("bar1")),
-// 		},
-// 		want: map[string]Value{
-// 			"": Map{}.
-// 				Set("foo0:s", String("bar0")).
-// 				Set("foo1:s", String("bar1")),
-// 			"foo0:s": String("bar0"),
-// 			"foo1:s": String("bar1"),
-// 		},
-// 	}, {
-// 		name: "should pass, simple map, stop after 2",
-// 		args: args{
-// 			v: Map{}.
-// 				Set("foo0:s", String("bar0")).
-// 				Set("foo1:s", String("bar1")),
-// 		},
-// 		stopAfter: 2,
-// 		want: map[string]Value{
-// 			"": Map{}.
-// 				Set("foo0:s", String("bar0")).
-// 				Set("foo1:s", String("bar1")),
-// 			"foo0:s": String("bar0"),
-// 		},
-// 	}, {
-// 		name: "should pass, complex map, stop after 6",
-// 		args: args{
-// 			v: Map{}.
-// 				Set("foo0:s", String("bar0")).
-// 				Set("foo1:s", String("bar1")).
-// 				Set("foo2:as", List{}.
-// 					Append(Int(1)).
-// 					Append(Int(2)).
-// 					Append(Int(3)),
-// 				).
-// 				Set("foo3:i", Int(4)),
-// 		},
-// 		stopAfter: 6,
-// 		want: map[string]Value{
-// 			"": Map{}.
-// 				Set("foo0:s", String("bar0")).
-// 				Set("foo1:s", String("bar1")).
-// 				Set("foo2:as", List{}.
-// 					Append(Int(1)).
-// 					Append(Int(2)).
-// 					Append(Int(3)),
-// 				).
-// 				Set("foo3:i", Int(4)),
-// 			"foo0:s": String("bar0"),
-// 			"foo1:s": String("bar1"),
-// 			"foo2:as": List{}.
-// 				Append(Int(1)).
-// 				Append(Int(2)).
-// 				Append(Int(3)),
-// 			"foo2:as/0": Int(1),
-// 			"foo2:as/1": Int(2),
-// 		},
-// 	}, {
-// 		name: "should pass, nested map, stop after 6",
-// 		args: args{
-// 			v: Map{}.
-// 				Set("foo0:s", String("bar0")).
-// 				Set("foo1:s", String("bar1")).
-// 				Set("foo2:am", Map{}.
-// 					Set("foo1:i", Int(1)).
-// 					Set("foo2:i", Int(2)).
-// 					Set("foo3:i", Int(3)),
-// 				).
-// 				Set("foo3:i", Int(4)),
-// 		},
-// 		stopAfter: 6,
-// 		want: map[string]Value{
-// 			"": Map{}.
-// 				Set("foo0:s", String("bar0")).
-// 				Set("foo1:s", String("bar1")).
-// 				Set("foo2:am", Map{}.
-// 					Set("foo1:i", Int(1)).
-// 					Set("foo2:i", Int(2)).
-// 					Set("foo3:i", Int(3)),
-// 				).
-// 				Set("foo3:i", Int(4)),
-// 			"foo0:s": String("bar0"),
-// 			"foo1:s": String("bar1"),
-// 			"foo2:am": Map{}.
-// 				Set("foo1:i", Int(1)).
-// 				Set("foo2:i", Int(2)).
-// 				Set("foo3:i", Int(3)),
-// 			"foo2:am/foo1:i": Int(1),
-// 			"foo2:am/foo2:i": Int(2),
-// 		},
-// 	}}
-// 	for _, tt := range tests {
-// 		tt := tt
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			res := map[string]Value{}
-// 			Traverse(tt.args.v, func(k string, v Value) bool {
-// 				res[k] = v
-// 				if tt.stopAfter > 0 && len(res) == tt.stopAfter {
-// 					return false
-// 				}
-// 				return true
-// 			})
-// 			assert.Equal(t, tt.want, res)
-// 		})
-// 	}
-// }
+func Test_Traverse(t *testing.T) {
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]interface{}
+	}{{
+		name: "should pass, simple value",
+		args: args{
+			1,
+		},
+		want: map[string]interface{}{
+			"": 1,
+		},
+	}, {
+		name: "should pass, simple list",
+		args: args{
+			v: []int64{0, 1, 2},
+		},
+		want: map[string]interface{}{
+			"":  []int64{0, 1, 2},
+			"0": int64(0),
+			"1": int64(1),
+			"2": int64(2),
+		},
+	}, {
+		name: "should pass, simple map",
+		args: args{
+			v: map[string]interface{}{
+				"foo0:s": "bar0",
+				"foo1:s": "bar1",
+			},
+		},
+		want: map[string]interface{}{
+			"": map[string]interface{}{
+				"foo0:s": "bar0",
+				"foo1:s": "bar1",
+			},
+			"foo0:s": "bar0",
+			"foo1:s": "bar1",
+		},
+	}, {
+
+		name: "should pass, complex map",
+		args: args{
+			v: map[string]interface{}{
+				"foo0:s":  "bar0",
+				"foo1:s":  "bar1",
+				"foo2:as": []int64{1, 2, 3},
+				"foo3:i":  4,
+			},
+		},
+		want: map[string]interface{}{
+			"": map[string]interface{}{
+				"foo0:s":  "bar0",
+				"foo1:s":  "bar1",
+				"foo2:as": []int64{1, 2, 3},
+				"foo3:i":  4,
+			},
+			"foo0:s":    "bar0",
+			"foo1:s":    "bar1",
+			"foo2:as":   []int64{1, 2, 3},
+			"foo2:as/0": int64(1),
+			"foo2:as/1": int64(2),
+			"foo2:as/2": int64(3),
+			"foo3:i":    4,
+		},
+	}, {
+		name: "should pass, nested map",
+		args: args{
+			v: map[string]interface{}{
+				"foo0:s": "bar0",
+				"foo1:s": "bar1",
+				"foo2:am": map[string]interface{}{
+					"foo1:i": int64(1),
+					"foo2:i": int64(2),
+					"foo3:i": int64(3),
+				},
+				"foo3:i": int64(4),
+			},
+		},
+		want: map[string]interface{}{
+			"": map[string]interface{}{
+				"foo0:s": "bar0",
+				"foo1:s": "bar1",
+				"foo2:am": map[string]interface{}{
+					"foo1:i": int64(1),
+					"foo2:i": int64(2),
+					"foo3:i": int64(3),
+				},
+				"foo3:i": int64(4),
+			},
+			"foo0:s": "bar0",
+			"foo1:s": "bar1",
+			"foo2:am": map[string]interface{}{
+				"foo1:i": int64(1),
+				"foo2:i": int64(2),
+				"foo3:i": int64(3),
+			},
+			"foo2:am/foo1:i": int64(1),
+			"foo2:am/foo2:i": int64(2),
+			"foo2:am/foo3:i": int64(3),
+			"foo3:i":         int64(4),
+		},
+	}}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			res := map[string]interface{}{}
+			Traverse(tt.args.v, func(k string, v interface{}) bool {
+				res[k] = v
+				return true
+			})
+			assert.Equal(t, tt.want, res)
+		})
+	}
+}
