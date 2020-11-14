0ad751231773f6411c1f37b6933db4e46a0f9a9c
George Antoniadis <george@noodles.gr>
2020-11-14 17:41:08 +0000

    chore(object): remove unused DeduceTypeHint method

--- pkg/object/type_hint.go
+++ pkg/object/type_hint.go
@@ -1,9 +1,5 @@
 package object
 
-import (
-	"reflect"
-)
-
 type (
 	// TypeHint are the hints of a member's type
 	TypeHint string
@@ -48,74 +44,9 @@ var (
 
 // GetTypeHint returns a TypeHint from a string
 func GetTypeHint(t string) TypeHint {
+	h := HintUndefined
 	if t, ok := hints[t]; ok {
-		return t
+		h = t
 	}
-	return HintUndefined
-}
-
-// DeduceTypeHint returns a TypeHint from a given value
-func DeduceTypeHint(o interface{}) TypeHint {
-	if o == nil {
-		return HintUndefined
-	}
-
-	t := reflect.TypeOf(o)
-	if t == nil {
-		return HintUndefined
-	}
-
-	if t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-
-	switch t.Kind() {
-	case reflect.Slice, reflect.Array:
-		if t.Elem() == reflect.TypeOf(byte(0)) {
-			return HintData
-		}
-		// TODO(geoah) add support for aa*
-		// eg {"foo:aai:" [["1", "2"], ["3", "4"]]} should be aai
-		sv := reflect.New(t.Elem()).Elem().Interface()
-		if sv == nil {
-			oo := o.([]interface{})
-			if len(oo) > 0 {
-				sv = oo[0]
-			}
-		}
-		if sv != nil {
-			subType := DeduceTypeHint(sv)
-			return HintArray + subType
-		}
-		return HintArray + "?" // TODO(geoah) should this return "" or panic maybe?
-
-	case reflect.String:
-		return HintString
-
-	case reflect.Map,
-		reflect.Struct:
-		return HintMap
-
-	case reflect.Float32, reflect.Float64:
-		return HintFloat
-
-	case reflect.Int,
-		reflect.Int8,
-		reflect.Int16,
-		reflect.Int32,
-		reflect.Int64:
-		return HintInt
-
-	case reflect.Uint,
-		reflect.Uint8,
-		reflect.Uint16,
-		reflect.Uint32,
-		reflect.Uint64:
-		return HintUint
-
-	case reflect.Bool:
-		return HintBool
-	}
-
-	return HintUndefined
+	return h
 }
