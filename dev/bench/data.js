76f9e568f7b8470e9a60529bfc132444d513c108
github-action-benchmark <github@users.noreply.github.com>
2020-11-18 02:31:40 +0000

    add Benchmark (go) benchmark result for de29b7481f5ccf23ee0a361ebf4b8bca53081cf2

---
+++ dev/bench/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1605666700843,
+  "repoUrl": "https://github.com/nimona/go-nimona",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "george@noodles.gr",
+            "name": "George Antoniadis",
+            "username": "geoah"
+          },
+          "committer": {
+            "email": "george@noodles.gr",
+            "name": "George Antoniadis",
+            "username": "geoah"
+          },
+          "distinct": true,
+          "id": "de29b7481f5ccf23ee0a361ebf4b8bca53081cf2",
+          "message": "chore(makefile): use gh-pages to store benchmark results",
+          "timestamp": "2020-11-18T02:29:45Z",
+          "tree_id": "3752a3939b6e88b80578b59c5001dadc7517f392",
+          "url": "https://github.com/nimona/go-nimona/commit/de29b7481f5ccf23ee0a361ebf4b8bca53081cf2"
+        },
+        "date": 1605666700352,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkToBlob1",
+            "value": 645390,
+            "unit": "ns/op",
+            "extra": "1658 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkToBlob100",
+            "value": 127499798,
+            "unit": "ns/op",
+            "extra": "8 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkToBlob1000",
+            "value": 10459145082,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkHash",
+            "value": 139239,
+            "unit": "ns/op",
+            "extra": "8443 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkEncode",
+            "value": 83.9,
+            "unit": "ns/op",
+            "extra": "14145715 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
