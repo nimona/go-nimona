5bc2b5c04bbb5b41306c6392b6a8f5ca2e2aabc9
Paris Goudas <decimal@darkstar.gr>
2020-11-30 22:11:55 +0000

    feat(config): update extra (un)marshalling

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,26 +8,30 @@ import (
 	"path/filepath"
 
 	"github.com/kelseyhightower/envconfig"
+
 	"nimona.io/pkg/crypto"
 	"nimona.io/pkg/peer"
 )
 
 type Config struct {
-	Path     string
-	Filename string
+	Path     string `json:"path"`
+	Filename string `json:"filename"`
 	Peer     struct {
-		PrivateKey           crypto.PrivateKey `envconfig:"PRIVATE_KEY"`
-		BindAddress          string            `envconfig:"BIND_ADDRESS"`
-		Bootstraps           []peer.Shorthand  `envconfig:"BOOTSTRAPS"`
-		ListenOnLocalIPs     bool              `envconfig:"LISTEN_LOCAL"`
-		ListenOnPrivateIPs   bool              `envconfig:"LISTEN_PRIVATE"`
-		ListenOnExternalPort bool              `envconfig:"LISTEN_EXTERNAL_PORT"`
-	} `envconfig:"PEER"`
-	Extras map[string]interface{} `json:",omitempty"`
+		PrivateKey           crypto.PrivateKey `json:"privateKey" envconfig:"PRIVATE_KEY"`
+		BindAddress          string            `json:"bindAddress" envconfig:"BIND_ADDRESS"`
+		Bootstraps           []peer.Shorthand  `json:"bootstraps" envconfig:"BOOTSTRAPS"`
+		ListenOnLocalIPs     bool              `json:"listenLocalIPs" envconfig:"LISTEN_LOCAL"`
+		ListenOnPrivateIPs   bool              `json:"listenPrivateIPs" envconfig:"LISTEN_PRIVATE"`
+		ListenOnExternalPort bool              `json:"listenExternalPort" envconfig:"LISTEN_EXTERNAL_PORT"`
+	} `json:"peer" envconfig:"PEER"`
+	Extras map[string]json.RawMessage `json:"extras,omitempty"`
+	extras map[string]interface{}
 }
 
 func New(opts ...Option) (*Config, error) {
-	cfg := &Config{}
+	cfg := &Config{
+		Extras: map[string]json.RawMessage{},
+	}
 	for _, opt := range opts {
 		opt(cfg)
 	}
@@ -51,17 +55,40 @@ func New(opts ...Option) (*Config, error) {
 		if err := json.Unmarshal(data, cfg); err != nil {
 			return nil, err
 		}
+		for k, r := range cfg.Extras {
+			target, ok := cfg.extras[k]
+			if !ok {
+				continue
+			}
+			if err := json.Unmarshal(r, target); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	cfg.setDefaults()
+
+	for k, r := range cfg.extras {
+		data, err := json.MarshalIndent(r, "", "  ")
+		if err != nil {
+			return nil, err
+		}
+		cfg.Extras[k] = data
+	}
+
+	updateData, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return nil, err
 	}
 
 	if err := envconfig.Process("nimona", cfg); err != nil {
 		return nil, err
 	}
 
-	cfg.setDefaults()
-
-	updateData, err := json.Marshal(cfg)
-	if err != nil {
-		return nil, err
+	for k, r := range cfg.extras {
+		if err := envconfig.Process("nimona_"+k, r); err != nil {
+			return nil, err
+		}
 	}
 
 	if err := ioutil.WriteFile(fullPath, updateData, 0600); err != nil {
@@ -88,9 +115,9 @@ func (cfg *Config) setDefaults() {
 	}
 	if len(cfg.Peer.Bootstraps) == 0 {
 		cfg.Peer.Bootstraps = []peer.Shorthand{
-			"ed25519.CJi6yjjXuNBFDoYYPrp697d6RmpXeW8ZUZPmEce9AgEc@tcps:asimov.bootstrap.nimona.io:22581",
-			"ed25519.6fVWVAK2DVGxBhtVBvzNWNKBWk9S83aQrAqGJfrxr75o@tcps:egan.bootstrap.nimona.io:22581",
-			"ed25519.7q7YpmPNQmvSCEBWW8ENw8XV8MHzETLostJTYKeaRTcL@tcps:sloan.bootstrap.nimona.io:22581",
+			"ed25519.CJi6yjjXuNBFDoYYPrp697d6RmpXeW8ZUZPmEce9AgEc@tcps:asimov.bootstrap.nimona.io:22581", // nolint: lll
+			"ed25519.6fVWVAK2DVGxBhtVBvzNWNKBWk9S83aQrAqGJfrxr75o@tcps:egan.bootstrap.nimona.io:22581",   // nolint: lll
+			"ed25519.7q7YpmPNQmvSCEBWW8ENw8XV8MHzETLostJTYKeaRTcL@tcps:sloan.bootstrap.nimona.io:22581",  // nolint: lll
 		}
 	}
 }
