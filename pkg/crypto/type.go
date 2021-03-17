7e68cfe9b3188a4d29117544a0321e900ed0f466
George Antoniadis <george@noodles.gr>
2021-03-17 23:30:01 +0000

    refactor(crypto): replace string based keys

--- pkg/crypto/type.go
+++
@@ -1,9 +0,0 @@
-package crypto
-
-// Supported values for KeyType
-const (
-	EC             = "EC"  // Elliptic Curve
-	InvalidKeyType = ""    // Invalid KeyType
-	OctetSeq       = "oct" // Octet sequence (used to represent symmetric keys)
-	RSA            = "RSA" // RSA
-)
