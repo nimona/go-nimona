538255420ce2d241ddb374e8dea92d0e6df5c916
George Antoniadis <george@noodles.gr>
2021-03-17 21:45:49 +0000

    chore(testutils): add NewTestBootstrapPeer

---
+++ pkg/testutils/hyperspace_provider.go
@@ -0,0 +1,58 @@
+package testutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"nimona.io/pkg/context"
+	"nimona.io/pkg/crypto"
+	"nimona.io/pkg/hyperspace/provider"
+	"nimona.io/pkg/localpeer"
+	"nimona.io/pkg/network"
+	"nimona.io/pkg/peer"
+)
+
+func NewTestBootstrapPeer(t *testing.T) *peer.ConnectionInfo {
+	t.Helper()
+
+	ctx := context.New(
+		context.WithCorrelationID("test-bootstrap"),
+	)
+
+	peerKey, err := crypto.GenerateEd25519PrivateKey()
+	require.NoError(t, err)
+
+	local := localpeer.New()
+	local.PutPrimaryPeerKey(peerKey)
+
+	// construct new network
+	net := network.New(
+		ctx,
+		network.WithLocalPeer(local),
+	)
+
+	// start listening
+	lis, err := net.Listen(
+		ctx,
+		"0.0.0.0:0",
+		network.ListenOnLocalIPs,
+		network.ListenOnPrivateIPs,
+	)
+	require.NoError(t, err)
+
+	// construct new hyperspace provider
+	_, err = provider.New(
+		ctx,
+		net,
+		nil,
+	)
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		err := lis.Close()
+		require.NoError(t, err)
+	})
+
+	return local.ConnectionInfo()
+}
