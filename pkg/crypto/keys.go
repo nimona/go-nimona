a685af1fb836f7e7f637920b3ba732a2ea72c7ae
George Antoniadis <george@noodles.gr>
2021-03-18 23:58:21 +0000

    refactor(crypto): refactor crypto methods to play nice with struct based keys

--- pkg/crypto/keys.go
+++ pkg/crypto/keys.go
@@ -1,24 +1,25 @@
 package crypto
 
 type (
-	KeyType   uint64
-	PublicKey interface {
-		// Equals(PublicKey) bool
-		// Raw() ([]byte, error)
-		Type() KeyType
-		String() string
-		MarshalString() (string, error)
-		UnmarshalString(string) error
-	}
-	PrivateKey interface {
-		PublicKey
-		PublicKey() PublicKey
-	}
+	KeyType      uint64
+	KeyAlgorithm uint64
+	// PublicKey interface {
+	// 	// Equals(PublicKey) bool
+	// 	// Raw() ([]byte, error)
+	// 	Type() KeyType
+	// 	String() string
+	// 	MarshalString() (string, error)
+	// 	UnmarshalString(string) error
+	// }
+	// PrivateKey interface {
+	// 	PublicKey
+	// 	PublicKey() PublicKey
+	// }
 )
 
 const (
-	cidEd25519Private = 0x1300 // well known value
-	cidEd25519Public  = 0xED   // well known value
+	Ed25519Private KeyAlgorithm = 0x1300 // well known value
+	Ed25519Public  KeyAlgorithm = 0xED   // well known value
 
 	PeerKey     KeyType = 0x6E00 // codec code for nimona _peer_ key
 	IdentityKey KeyType = 0x6E01 // codec code for nimona _identity_ key
