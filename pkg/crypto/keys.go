f16093f445ffbb343a2332ead7248934ce7946c6
George Antoniadis <george@noodles.gr>
2021-03-26 15:46:45 +0000

    refactor(crypto): rename KeyType to KeyUsage

--- pkg/crypto/keys.go
+++ pkg/crypto/keys.go
@@ -1,26 +1,14 @@
 package crypto
 
 type (
-	KeyType      uint64
+	KeyUsage     uint64
 	KeyAlgorithm uint64
-	// PublicKey interface {
-	// 	// Equals(PublicKey) bool
-	// 	// Raw() ([]byte, error)
-	// 	Type() KeyType
-	// 	String() string
-	// 	MarshalString() (string, error)
-	// 	UnmarshalString(string) error
-	// }
-	// PrivateKey interface {
-	// 	PublicKey
-	// 	PublicKey() PublicKey
-	// }
 )
 
 const (
 	Ed25519Private KeyAlgorithm = 0x1300 // well known value
 	Ed25519Public  KeyAlgorithm = 0xED   // well known value
 
-	PeerKey     KeyType = 0x6E00 // codec code for nimona _peer_ key
-	IdentityKey KeyType = 0x6E01 // codec code for nimona _identity_ key
+	PeerKey     KeyUsage = 0x6E00 // codec code for nimona _peer_ key
+	IdentityKey KeyUsage = 0x6E01 // codec code for nimona _identity_ key
 )
