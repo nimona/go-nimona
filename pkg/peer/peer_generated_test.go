1d11ae38906a8183a4c7d0813731ff1c711b8593
George Antoniadis <george@noodles.gr>
2021-04-29 22:25:01 +0100

    chore(peer): cleanup peer files

--- pkg/peer/peer_generated_test.go
+++ pkg/peer/peer_generated_test.go
@@ -2,12 +2,38 @@ package peer
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"nimona.io/pkg/crypto"
+	object "nimona.io/pkg/object"
 )
 
-func Test(t *testing.T) {
-	c := &ConnectionInfo{}
-	b, _ := json.Marshal(c.ToObject().ToMap())
-	fmt.Println(string(b))
+func TestEncoding(t *testing.T) {
+	k, err := crypto.NewEd25519PrivateKey(crypto.PeerKey)
+	require.NoError(t, err)
+
+	c := &ConnectionInfo{
+		Metadata: object.Metadata{
+			Owner:    k.PublicKey(),
+			Datetime: time.Now().Format(time.RFC3339),
+		},
+		Version:       1,
+		PublicKey:     k.PublicKey(),
+		Addresses:     []string{"foo", "bar"},
+		ObjectFormats: []string{"foobar"},
+	}
+	b, err := json.Marshal(c.ToObject())
+	require.NoError(t, err)
+
+	g := &object.Object{}
+	err = json.Unmarshal(b, g)
+	require.NoError(t, err)
+
+	r := &ConnectionInfo{}
+	err = r.FromObject(g)
+	require.NoError(t, err)
+
+	require.Equal(t, c, r)
 }
