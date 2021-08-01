f8bd0f47cad1fde04591a65c73d8ca2e323339bc
George Antoniadis <george@noodles.gr>
2021-08-01 15:53:48 +0100

    feat(object): add metadata.sequence

--- pkg/object/metadata.go
+++ pkg/object/metadata.go
@@ -7,7 +7,7 @@ import (
 
 type (
 	// Metadata for object
-	// TODO: add shape, sequence
+	// TODO: add shape
 	// TODO: add authors, contributors, license, copyright
 	// TODO: add version
 	Metadata struct {
@@ -15,6 +15,7 @@ type (
 		Parents   Parents    `nimona:"parents:m"`
 		Policies  Policies   `nimona:"policies:am"`
 		Root      chore.Hash `nimona:"root:r"`
+		Sequence  uint64     `nimona:"sequence:u"`
 		Signature Signature  `nimona:"_signature:m"`
 		Timestamp string     `nimona:"timestamp:s"`
 	}
