d0b01afe1e920b4c5cb68cb235c14ef9b0b8c8dd
George Antoniadis <george@noodles.gr>
2021-08-07 12:31:45 +0100

    chore(keystream): add test comments

--- pkg/keystream/controller_test.go
+++ pkg/keystream/controller_test.go
@@ -26,30 +26,37 @@ func TestController_New(t *testing.T) {
 	objectStore, err := sqlobjectstore.New(objectStoreDB)
 	require.NoError(t, err)
 
+	// create a controller with empty stores
 	ctrl, err := NewController(kvStore, objectStore)
 	require.NoError(t, err)
 	require.NotNil(t, ctrl)
 
+	// get active and next keys
 	wantActiveKey := ctrl.currentPrivateKey.PublicKey()
 	wantNextKeyDigest := ctrl.state.NextKeyDigest
 
+	// create a new controller with the now not empty stores
 	ctrl2, err := NewController(kvStore, objectStore)
 	require.NoError(t, err)
 	require.NotNil(t, ctrl2)
 
+	// get active and next keys
 	gotActiveKey := ctrl2.currentPrivateKey.PublicKey()
 	gotNextKeyHash := ctrl2.state.NextKeyDigest
 
 	require.NotEmpty(t, gotActiveKey)
 	require.NotEmpty(t, gotNextKeyHash)
 
+	// and make sure they are the same
 	require.Equal(t, wantActiveKey, gotActiveKey)
 	require.Equal(t, wantNextKeyDigest, gotNextKeyHash)
 
+	// rotate the keys on the latest controller
 	rotationEvent, err := ctrl2.Rotate()
 	require.NoError(t, err)
 	require.NotNil(t, rotationEvent)
 
+	// and check the rotation worked
 	require.Equal(t, wantNextKeyDigest, getPublicKeyHash(rotationEvent.Key))
 
 	gotActiveKey = ctrl2.currentPrivateKey.PublicKey()
