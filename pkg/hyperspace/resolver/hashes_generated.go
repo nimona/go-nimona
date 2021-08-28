ba5c7ba8a672c21b1ba156ea8654141eebbec2cc
George Antoniadis <george@noodles.gr>
2021-08-28 20:40:36 +0100

    chore(tilde): chore > tilde in ndl files

--- pkg/hyperspace/resolver/hashes_generated.go
+++ pkg/hyperspace/resolver/hashes_generated.go
@@ -11,37 +11,37 @@ import (
 )
 
 type (
-	// ChoreHashSyncList -
-	ChoreHashSyncList struct {
+	// TildeDigestSyncList -
+	TildeDigestSyncList struct {
 		m sync.Map
 	}
 )
 
 // Put -
-func (m *ChoreHashSyncList) Put(k tilde.Digest) {
+func (m *TildeDigestSyncList) Put(k tilde.Digest) {
 	m.m.Store(k, true)
 }
 
 // Exists -
-func (m *ChoreHashSyncList) Exists(k tilde.Digest) bool {
+func (m *TildeDigestSyncList) Exists(k tilde.Digest) bool {
 	_, ok := m.m.Load(k)
 	return ok
 }
 
 // Delete -
-func (m *ChoreHashSyncList) Delete(k tilde.Digest) {
+func (m *TildeDigestSyncList) Delete(k tilde.Digest) {
 	m.m.Delete(k)
 }
 
 // Range -
-func (m *ChoreHashSyncList) Range(i func(k tilde.Digest) bool) {
+func (m *TildeDigestSyncList) Range(i func(k tilde.Digest) bool) {
 	m.m.Range(func(k, v interface{}) bool {
 		return i(k.(tilde.Digest))
 	})
 }
 
 // List -
-func (m *ChoreHashSyncList) List() []tilde.Digest {
+func (m *TildeDigestSyncList) List() []tilde.Digest {
 	r := []tilde.Digest{}
 	m.m.Range(func(k, v interface{}) bool {
 		r = append(r, k.(tilde.Digest))
