ab3f69b5c0a4e2db21c860319979cdaf3693cfe9
George Antoniadis <george@noodles.gr>
2022-03-07 00:25:53 +0000

    feat(resolver): add composite resolver

--- pkg/resolver/resolver.go
+++ pkg/resolver/resolver.go
@@ -24,3 +24,49 @@ type Resolver interface {
 		cid tilde.Digest,
 	) ([]*peer.ConnectionInfo, error)
 }
+
+type CompositeResolver struct {
+	Resolvers []Resolver
+	mutex     sync.RWMutex
+}
+
+// New creates a new composite resolver.
+func New(resolvers ...Resolver) *CompositeResolver {
+	return &CompositeResolver{
+		Resolvers: resolvers,
+	}
+}
+
+func (r *CompositeResolver) LookupByDID(
+	ctx context.Context,
+	id did.DID,
+) ([]*peer.ConnectionInfo, error) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+	for _, r := range r.Resolvers {
+		if c, err := r.LookupByDID(ctx, id); err == nil {
+			return c, nil
+		}
+	}
+	return nil, ErrNotFound
+}
+
+func (r *CompositeResolver) LookupByContent(
+	ctx context.Context,
+	cid tilde.Digest,
+) ([]*peer.ConnectionInfo, error) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+	for _, r := range r.Resolvers {
+		if c, err := r.LookupByContent(ctx, cid); err == nil {
+			return c, nil
+		}
+	}
+	return nil, ErrNotFound
+}
+
+func (r *CompositeResolver) RegisterResolver(resolver Resolver) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	r.Resolvers = append(r.Resolvers, resolver)
+}
