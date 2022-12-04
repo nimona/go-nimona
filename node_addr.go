3c04a2c4a22de24fe007218947cd93509bb28df1
George Antoniadis <george@noodles.gr>
2022-12-04 23:01:13 +0000

    refactor(node): update string node address

--- node_addr.go
+++ node_addr.go
@@ -1,11 +1,17 @@
 package nimona
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 type NodeAddr struct {
 	host      string
 	port      int
 	transport string
+	extra     string
 }
 
 func (a NodeAddr) Address() string {
@@ -20,7 +26,40 @@ func (a NodeAddr) Network() string {
 }
 
 func (a NodeAddr) String() string {
-	return fmt.Sprintf("%s://%s:%d", a.transport, a.host, a.port)
+	return fmt.Sprintf("nimona://%s:%s:%d", a.transport, a.host, a.port)
+}
+
+func (a *NodeAddr) Parse(addr string) error {
+	if !strings.HasPrefix(addr, "nimona://") {
+		return errors.New("unsupported scheme")
+	}
+
+	addr = strings.TrimPrefix(addr, "nimona://")
+
+	transport, addr, ok := strings.Cut(addr, ":")
+	if !ok {
+		return errors.New("invalid address, can't find transport")
+	}
+
+	host, addr, ok := strings.Cut(addr, ":")
+	if !ok {
+		return errors.New("invalid address, can't find host")
+	}
+
+	extra := ""
+	portStr, extra, _ := strings.Cut(addr, "/")
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return errors.New("invalid port")
+	}
+
+	a.host = host
+	a.port = port
+	a.transport = transport
+	a.extra = extra
+
+	return nil
 }
 
 func NewNodeAddr(transport, host string, port int) NodeAddr {
--- node_addr_test.go
+++ node_addr_test.go
@@ -8,7 +8,29 @@ import (
 
 func TestNodeAddr(t *testing.T) {
 	a := NewNodeAddr("utp", "localhost", 1234)
-	require.Equal(t, "utp", a.Network())
-	require.Equal(t, "localhost:1234", a.Address())
-	require.Equal(t, "utp://localhost:1234", a.String())
+
+	t.Run("struct addr to string", func(t *testing.T) {
+		require.Equal(t, "utp", a.Network())
+		require.Equal(t, "localhost:1234", a.Address())
+		require.Equal(t, "nimona://utp:localhost:1234", a.String())
+	})
+
+	t.Run("string addr to struct", func(t *testing.T) {
+		g := NodeAddr{}
+		err := g.Parse(a.String())
+		require.NoError(t, err)
+		require.Equal(t, a, g)
+	})
+
+	t.Run("string addr to struct with extras", func(t *testing.T) {
+		g := NodeAddr{}
+		err := g.Parse("nimona://utp:localhost:1234/something")
+		require.NoError(t, err)
+		require.Equal(t, NodeAddr{
+			host:      "localhost",
+			port:      1234,
+			transport: "utp",
+			extra:     "something",
+		}, g)
+	})
 }
