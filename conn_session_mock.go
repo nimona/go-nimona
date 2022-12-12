030e766a982f293d2fd52a2ccbba76216f6fc7a7
George Antoniadis <george@noodles.gr>
2022-12-12 22:45:26 +0000

    feat(conn/session): add MockSession

---
+++ conn_session_mock.go
@@ -0,0 +1,55 @@
+package nimona
+
+import (
+	"io"
+	"testing"
+
+	"github.com/oasisprotocol/curve25519-voi/primitives/ed25519"
+	"github.com/stretchr/testify/require"
+)
+
+type MockSession struct {
+	Server *Session
+	Client *Session
+}
+
+func NewMockSession(t *testing.T) *MockSession {
+	t.Helper()
+
+	sr, cw := io.Pipe()
+	cr, sw := io.Pipe()
+
+	clientConn := &MockConnEndpoint{
+		Reader: sr,
+		Writer: sw,
+	}
+
+	serverConn := &MockConnEndpoint{
+		Reader: cr,
+		Writer: cw,
+	}
+
+	m := &MockSession{
+		Server: NewSession(clientConn),
+		Client: NewSession(serverConn),
+	}
+
+	serverPublic, serverPrivate, err := ed25519.GenerateKey(nil)
+	require.NoError(t, err)
+
+	clientPublic, clientPrivate, err := ed25519.GenerateKey(nil)
+	require.NoError(t, err)
+
+	serverDone := make(chan struct{})
+
+	go func() {
+		err := m.Server.DoServer(serverPublic, serverPrivate)
+		require.NoError(t, err)
+		close(serverDone)
+	}()
+	err = m.Client.DoClient(clientPublic, clientPrivate)
+	require.NoError(t, err)
+
+	<-serverDone
+	return m
+}
