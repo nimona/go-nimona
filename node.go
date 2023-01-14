90d459c75b658f54ad552887dd2a8dd5fca4a0dc
George Antoniadis <george@noodles.gr>
2023-01-14 01:58:18 +0000

    feat(node): add basic Node with ability to join and list networks

---
+++ node.go
@@ -0,0 +1,126 @@
+package nimona
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/oasisprotocol/curve25519-voi/primitives/ed25519"
+
+	"nimona.io/internal/xsync"
+)
+
+type (
+	Node struct {
+		config   NodeConfig
+		sessions *SessionManager
+		networks *xsync.Map[NetworkID, nodeNetwork]
+	}
+	nodeNetwork struct {
+		networkInfo NetworkInfo
+	}
+	NodeConfig struct {
+		Dialer        Dialer
+		Listener      Listener
+		Resolver      Resolver
+		PublicKey     ed25519.PublicKey
+		PrivateKey    ed25519.PrivateKey
+		Handlers      map[string]RequestHandlerFunc
+		DocumentStore *DocumentStore
+	}
+)
+
+func NewNode(cfg NodeConfig) (*Node, error) {
+	ses, err := NewSessionManager(
+		cfg.Dialer,
+		cfg.Listener,
+		cfg.PublicKey,
+		cfg.PrivateKey,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error creating session manager: %w", err)
+	}
+
+	for docType, handler := range cfg.Handlers {
+		ses.RegisterHandler(docType, handler)
+	}
+
+	n := &Node{
+		config:   cfg,
+		sessions: ses,
+		networks: xsync.NewMap[NetworkID, nodeNetwork](),
+	}
+
+	return n, nil
+}
+
+func (n *Node) ListNetworks() ([]NetworkInfo, error) {
+	docs, err := n.config.DocumentStore.GetDocumentsByType("core/network/info")
+	if err != nil {
+		return nil, fmt.Errorf("error getting documents: %w", err)
+	}
+
+	var networks []NetworkInfo
+	for _, doc := range docs {
+		netInfo := &NetworkInfo{}
+		err := doc.UnmarshalInto(netInfo)
+		if err != nil {
+			return nil, fmt.Errorf("error unmarshaling document: %w", err)
+		}
+		networks = append(networks, *netInfo)
+	}
+
+	return networks, nil
+}
+
+func (n *Node) JoinNetwork(ctx context.Context, nID NetworkID) (*NetworkInfo, error) {
+	peerAddrs, err := n.config.Resolver.Resolve(nID)
+	if err != nil {
+		return nil, fmt.Errorf("error resolving network: %w", err)
+	}
+
+	var errs error
+	var netInfo *NetworkInfo
+	for _, peerAddr := range peerAddrs {
+		ses, err := n.sessions.Dial(ctx, peerAddr)
+		if err != nil {
+			errs = multierror.Append(errs, err)
+			continue
+		}
+		netInfo, err = RequestNetworkInfo(ctx, ses)
+		if err != nil {
+			errs = multierror.Append(errs, err)
+			continue
+		}
+		break
+	}
+
+	if errs != nil {
+		return nil, fmt.Errorf("error joining network: %w", errs)
+	}
+
+	if netInfo == nil {
+		return nil, fmt.Errorf("missing response when joining network: %w", errs)
+	}
+
+	docID := NewDocumentIDFromCBOR(netInfo.RawBytes)
+	err = n.config.DocumentStore.PutDocument(&DocumentEntry{
+		DocumentID:       docID,
+		DocumentType:     "core/network/info",
+		DocumentEncoding: "cbor",
+		DocumentBytes:    netInfo.RawBytes,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error storing network info: %w", err)
+	}
+
+	if netInfo == nil {
+		return nil, fmt.Errorf("error joining network: %w", errs)
+	}
+
+	return netInfo, nil
+}
+
+func (n *Node) Close() error {
+	return n.sessions.Close()
+}
---
+++ node_test.go
@@ -0,0 +1,77 @@
+package nimona
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/oasisprotocol/curve25519-voi/primitives/ed25519"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNode_E2E(t *testing.T) {
+	netHostname := NewNetworkID("testing.nimona.io")
+
+	// Setup "server" side
+
+	srvNodeConfig := NewTestNodeConfig(t)
+	srvHandlerNetwork := &HandlerNetwork{
+		Hostname: "testing.nimona.io",
+		PeerAddresses: []PeerAddr{
+			srvNodeConfig.Listener.PeerAddr(),
+		},
+	}
+	srvNodeConfig.Handlers = map[string]RequestHandlerFunc{
+		"core/network/info.request": srvHandlerNetwork.HandleNetworkInfoRequest,
+	}
+	_, err := NewNode(srvNodeConfig)
+	require.NoError(t, err)
+
+	// Setup "client" side
+
+	clnResolver := NewMockResolver(gomock.NewController(t))
+	clnResolver.EXPECT().
+		Resolve(netHostname).
+		Return([]PeerAddr{
+			srvNodeConfig.Listener.PeerAddr(),
+		}, nil)
+	clnNodeConfig := NewTestNodeConfig(t)
+	clnNodeConfig.Resolver = clnResolver
+	clnNode, err := NewNode(clnNodeConfig)
+	require.NoError(t, err)
+
+	t.Run("join network", func(t *testing.T) {
+		ctx := context.Background()
+		netInfo, err := clnNode.JoinNetwork(ctx, netHostname)
+		require.NoError(t, err)
+		require.Equal(t, netHostname, netInfo.NetworkID)
+		require.Equal(t, srvNodeConfig.Listener.PeerAddr(), netInfo.PeerAddresses[0])
+	})
+
+	t.Run("list networks", func(t *testing.T) {
+		netInfos, err := clnNode.ListNetworks()
+		require.NoError(t, err)
+		require.Len(t, netInfos, 1)
+		require.Equal(t, netHostname, netInfos[0].NetworkID)
+		require.Equal(t, srvNodeConfig.Listener.PeerAddr(), netInfos[0].PeerAddresses[0])
+	})
+}
+
+func NewTestNodeConfig(t *testing.T) NodeConfig {
+	t.Helper()
+
+	pub, prv, err := ed25519.GenerateKey(nil)
+	require.NoError(t, err)
+
+	transport := &TransportUTP{}
+	listener, err := transport.Listen(context.Background(), "127.0.0.1:0")
+	require.NoError(t, err)
+
+	return NodeConfig{
+		Dialer:        transport,
+		Listener:      listener,
+		PublicKey:     pub,
+		PrivateKey:    prv,
+		DocumentStore: NewTestDocumentStore(t),
+	}
+}
