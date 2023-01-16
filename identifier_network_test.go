4339ad6acb37fdbbcff37bd861868c9092bd499f
George Antoniadis <george@noodles.gr>
2023-01-16 18:35:47 +0000

    chore(identifier/network): add TestNetworkID/MarshalUnmarshal test

--- identifier_network_test.go
+++ identifier_network_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNetworkID(t *testing.T) {
+func TestNetworkID_ParseNetworkID(t *testing.T) {
 	s0 := "nimona://network:handle:testing.reamde.dev"
 	n0 := NetworkID{
 		Hostname: "testing.reamde.dev",
@@ -16,5 +16,20 @@ func TestNetworkID(t *testing.T) {
 
 	n1, err := ParseNetworkID(s0)
 	require.NoError(t, err)
+	require.Equal(t, n0, n1)
+}
+
+func TestNetworkID_MarshalUnmarshal(t *testing.T) {
+	n0 := NetworkID{
+		Hostname: "testing.reamde.dev",
+	}
+
+	b, err := n0.MarshalCBORBytes()
+	require.NoError(t, err)
+
+	var n1 NetworkID
+	err = n1.UnmarshalCBORBytes(b)
+	require.NoError(t, err)
+
 	require.Equal(t, n0, n1)
 }
