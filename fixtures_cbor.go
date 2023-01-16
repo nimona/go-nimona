5ee3b9caf8ed5f8dc1e6768a20e4f9cad74bebd4
George Antoniadis <george@noodles.gr>
2023-01-16 18:34:48 +0000

    chore(fixtures): add DocumentID to CborFixture

--- fixtures_cbor.go
+++ fixtures_cbor.go
@@ -1,17 +1,20 @@
 package nimona
 
 type CborFixture struct {
-	String         string       `cborgen:"string,omitempty" json:"string,omitempty"`
-	Uint64         uint64       `cborgen:"uint64,omitempty" json:"uint64,omitempty"`
-	Int64          int64        `cborgen:"int64,omitempty" json:"int64,omitempty"`
-	Bytes          []byte       `cborgen:"bytes,omitempty" json:"bytes,omitempty"`
-	Bool           bool         `cborgen:"bool,omitempty" json:"bool,omitempty"`
-	Map            *CborFixture `cborgen:"map,omitempty" json:"map,omitempty"`
-	RepeatedString []string     `cborgen:"repeatedstring,omitempty" json:"repeatedstring,omitempty"`
-	RepeatedUint64 []uint64     `cborgen:"repeateduint64,omitempty" json:"repeateduint64,omitempty"`
-	RepeatedInt64  []int64      `cborgen:"repeatedint64,omitempty" json:"repeatedint64,omitempty"`
-	RepeatedBytes  [][]byte     `cborgen:"repeatedbytes,omitempty" json:"repeatedbytes,omitempty"`
-	// RepeatedBool   []bool `cborgen:"repeatedbool,omitempty" json:"repeatedbool,omitempty"`
-	RepeatedMap     []*CborFixture `cborgen:"repeatedmap,omitempty" json:"repeatedmap,omitempty"`
-	EphemeralString string         `cborgen:"_ephemeralString,omitempty" json:"_ephemeralString,omitempty"`
+	String         string       `cborgen:"string,omitempty"`
+	Uint64         uint64       `cborgen:"uint64,omitempty"`
+	Int64          int64        `cborgen:"int64,omitempty"`
+	Bytes          []byte       `cborgen:"bytes,omitempty"`
+	Bool           bool         `cborgen:"bool,omitempty"`
+	Map            *CborFixture `cborgen:"map,omitempty"`
+	RepeatedString []string     `cborgen:"repeatedstring,omitempty"`
+	RepeatedUint64 []uint64     `cborgen:"repeateduint64,omitempty"`
+	RepeatedInt64  []int64      `cborgen:"repeatedint64,omitempty"`
+	RepeatedBytes  [][]byte     `cborgen:"repeatedbytes,omitempty"`
+	// RepeatedBool   []bool `cborgen:"repeatedbool,omitempty"`
+	RepeatedMap     []*CborFixture `cborgen:"repeatedmap,omitempty"`
+	EphemeralString string         `cborgen:"_ephemeralString,omitempty"`
+
+	// Tuples
+	DocumentID DocumentID `cborgen:"documentID,omitempty"`
 }
