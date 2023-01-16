b981a27130f4c5540a61832051e9934f51671a89
George Antoniadis <george@noodles.gr>
2023-01-16 18:32:23 +0000

    chore(identifier/document): add NewTestRandomDocumentID

--- identifier_document_test.go
+++ identifier_document_test.go
@@ -3,6 +3,7 @@ package nimona
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,4 +20,23 @@ func TestDocumentID_Marshal(t *testing.T) {
 	}
 
 	PrettyPrint(id)
+}
+
+func TestDocumentHash_NewTestRandomDocumentID(t *testing.T) {
+	t.Run("test random document id", func(t *testing.T) {
+		h1 := NewTestRandomDocumentID(t)
+		h2 := NewTestRandomDocumentID(t)
+		require.NotEqual(t, h1, h2)
+	})
+}
+
+// NewTestRandomDocumentID returns a random document ID for testing purposes.
+func NewTestRandomDocumentID(t *testing.T) DocumentID {
+	t.Helper()
+
+	doc := &CborFixture{
+		String: uuid.New().String(),
+	}
+
+	return NewDocumentID(doc)
 }
