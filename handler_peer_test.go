0e260e1ae9eac9a8ff1bee4d0843c87aa61c7679
George Antoniadis <george@noodles.gr>
2023-01-17 00:31:34 +0000

    chore(handler/peer): rename test TestHandlerNetworkCapabilities

--- handler_peer_test.go
+++ handler_peer_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRPCNetworkCapabilities(t *testing.T) {
+func TestHandlerNetworkCapabilities(t *testing.T) {
 	srv, clt := newTestSessionManager(t)
 
 	caps := []string{"core/peer/capabilities"}
