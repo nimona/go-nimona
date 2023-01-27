935eb875779d19c6bffd4ce36b3b6179d017ec97
George Antoniadis <george@noodles.gr>
2023-01-27 18:26:31 +0000

    feat(handler/network): add ability to join network

--- handler_network.go
+++ handler_network.go
@@ -2,6 +2,7 @@ package nimona
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
@@ -12,13 +13,13 @@ type (
 	NetworkInfoRequest struct {
 		_ string `cborgen:"$type,const=core/network/info.request"`
 	}
-	NetworkAccountingRegistrationRequest struct {
-		_               string   `cborgen:"$type,const=core/network/accounting/registration.request"`
+	NetworkJoinRequest struct {
+		_               string   `cborgen:"$type,const=core/network/join.request"`
 		Metadata        Metadata `cbor:"$metadata,omitempty"`
 		RequestedHandle string   `cbor:"requestedHandle,omitempty"`
 	}
-	NetworkAccountingRegistrationResponse struct {
-		_                string `cborgen:"$type,const=core/network/accounting/registration.response"`
+	NetworkJoinResponse struct {
+		_                string `cborgen:"$type,const=core/network/join.response"`
 		Handle           string `cbor:"handle,omitempty"`
 		Accepted         bool   `cbor:"accepted"`
 		Error            bool   `cbor:"error,omitempty"`
@@ -29,15 +30,15 @@ type (
 type (
 	HandlerNetwork struct {
 		Hostname        string
+		PeerConfig      *PeerConfig
 		PeerAddresses   []PeerAddr
-		PrivateKey      PrivateKey
 		AccountingStore *gorm.DB
 	}
 	NetworkAccountingModel struct {
-		Handle    string   `gorm:"primaryKey"`
-		PeerInfo  PeerInfo `gorm:"embedded"`
-		CreatedAt time.Time
-		UpdatedAt time.Time
+		Handle     string      `gorm:"uniqueIndex"`
+		IdentityID *IdentityID `gorm:"primaryKey"`
+		CreatedAt  time.Time
+		UpdatedAt  time.Time
 	}
 )
 
@@ -72,6 +73,101 @@ func (h *HandlerNetwork) HandleNetworkInfoRequest(
 			Hostname: h.Hostname,
 		},
 		PeerAddresses: h.PeerAddresses,
+	}
+	err = msg.Respond(res)
+	if err != nil {
+		return fmt.Errorf("error replying: %w", err)
+	}
+	return nil
+}
+
+func RequestNetworkJoin(
+	ctx context.Context,
+	ses *Session,
+	peerConfig *PeerConfig,
+	requestedHandle string,
+) (*NetworkJoinResponse, error) {
+	req := &NetworkJoinRequest{
+		RequestedHandle: requestedHandle,
+	}
+	identity := peerConfig.GetIdentity()
+	if identity == nil {
+		return nil, fmt.Errorf("cannot join a network without an identity")
+	}
+	id := identity.IdentityID()
+	req.Metadata.Owner = &id
+	res := &NetworkJoinResponse{}
+	msgRes, err := ses.Request(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending message: %w", err)
+	}
+	err = msgRes.Decode(res)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding message: %w", err)
+	}
+	return res, nil
+}
+
+func (h *HandlerNetwork) HandleNetworkJoinRequest(
+	ctx context.Context,
+	msg *Request,
+) error {
+	req := &NetworkJoinRequest{}
+	err := msg.Decode(req)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling request: %w", err)
+	}
+
+	respondWithError := func(desc string) error {
+		res := &NetworkJoinResponse{
+			Error:            true,
+			ErrorDescription: desc,
+		}
+		err = msg.Respond(res)
+		if err != nil {
+			return fmt.Errorf("error replying: %w", err)
+		}
+		return nil
+	}
+
+	if req.Metadata.Owner == nil {
+		return respondWithError("no owner specified")
+	}
+
+	if req.RequestedHandle == "" {
+		return respondWithError("no handle specified")
+	}
+
+	acc := &NetworkAccountingModel{}
+	que := h.AccountingStore.First(acc, "identity_id = ?", req.Metadata.Owner)
+	if que.Error == nil {
+		return respondWithError("already joined")
+	} else if errors.Is(que.Error, gorm.ErrRecordNotFound) {
+		// all ok
+	} else if que.Error != nil {
+		return respondWithError("temporary error, try again later")
+	}
+
+	que = h.AccountingStore.First(acc, "handle = ?", req.RequestedHandle)
+	if que.Error == nil {
+		return respondWithError("handle already taken")
+	} else if errors.Is(que.Error, gorm.ErrRecordNotFound) {
+		// all ok
+	} else if que.Error != nil {
+		return respondWithError("temporary error, try again later")
+	}
+
+	acc.Handle = req.RequestedHandle
+	acc.IdentityID = req.Metadata.Owner
+
+	que = h.AccountingStore.Create(acc)
+	if que.Error != nil {
+		return respondWithError("temporary error, try again later")
+	}
+
+	res := &NetworkJoinResponse{
+		Accepted: true,
+		Handle:   req.RequestedHandle,
 	}
 	err = msg.Respond(res)
 	if err != nil {
--- handler_network_test.go
+++ handler_network_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func TestHandlerNetwork(t *testing.T) {
@@ -41,4 +44,81 @@ func TestHandlerNetwork(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, info.NetworkAlias, res.NetworkAlias)
 	require.Equal(t, info.PeerAddresses, res.PeerAddresses)
+}
+
+func TestRequestNetworkJoin(t *testing.T) {
+	ctx := context.Background()
+
+	// Create new peer configs
+	srvPeerConfig := NewTestPeerConfig(t)
+	clnPeerConfig := NewTestPeerConfig(t)
+
+	// Create new session manager
+	srv, clt := newTestSessionManager(t)
+
+	// Construct a new HandlerNetwork
+	hnd := &HandlerNetwork{
+		Hostname: "testing.nimona.io",
+		PeerAddresses: []PeerAddr{{
+			Network: "utp",
+			Address: "localhost:1234",
+		}},
+		PeerConfig:      srvPeerConfig,
+		AccountingStore: NewTestNetworkStore(t),
+	}
+	srv.RegisterHandler(
+		"core/network/join.request",
+		hnd.HandleNetworkJoinRequest,
+	)
+
+	// Dial the server
+	ses, err := clt.Dial(context.Background(), srv.PeerAddr())
+	require.NoError(t, err)
+
+	// Test with missing identity
+	t.Run("missing identity", func(t *testing.T) {
+		_, err = RequestNetworkJoin(ctx, ses, clnPeerConfig, "test-handle")
+		require.Error(t, err)
+	})
+
+	// Create a new identity for the client
+	clnPeerConfig.SetIdentity(NewTestIdentity(t))
+
+	// Test with empty handle
+	t.Run("empty handle", func(t *testing.T) {
+		res, err := RequestNetworkJoin(ctx, ses, clnPeerConfig, "")
+		require.NoError(t, err)
+		require.False(t, res.Accepted)
+		require.NotEmpty(t, res.Error)
+	})
+
+	// Test with a valid identity and non existing handle
+	t.Run("valid identity and non existing handle", func(t *testing.T) {
+		res, err := RequestNetworkJoin(ctx, ses, clnPeerConfig, "test-handle")
+		require.NoError(t, err)
+		require.True(t, res.Accepted)
+		require.Equal(t, "test-handle", res.Handle)
+	})
+
+	// Test with an existing identity
+	t.Run("existing identity", func(t *testing.T) {
+		res, err := RequestNetworkJoin(ctx, ses, clnPeerConfig, "new-handle")
+		require.NoError(t, err)
+		require.False(t, res.Accepted)
+		require.NotEmpty(t, res.Error)
+	})
+}
+
+func NewTestNetworkStore(t *testing.T) *gorm.DB {
+	t.Helper()
+
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{
+		Logger: logger.Discard,
+	})
+	require.NoError(t, err)
+
+	err = db.AutoMigrate(&NetworkAccountingModel{})
+	require.NoError(t, err)
+
+	return db
 }
