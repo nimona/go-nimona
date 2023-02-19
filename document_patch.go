677809777388853923b66b814644d1e5138e1a76
George Antoniadis <george@noodles.gr>
2023-02-19 23:27:57 +0000

    chore(document/patch): add missing DocumentPatch

--- document_patch.go
+++ document_patch.go
@@ -1,1 +1,18 @@
 package nimona
+
+type (
+	DocumentPatch struct {
+		_            string                   `nimona:"$type,type=core/stream/patch"`
+		Metadata     Metadata                 `nimona:"$metadata,omitempty"`
+		Dependencies []DocumentID             `nimona:"dependencies,omitempty"`
+		Operations   []DocumentPatchOperation `nimona:"operations,omitempty"`
+	}
+	DocumentPatchOperation struct {
+		Op   string `nimona:"op"`
+		Path string `nimona:"path"`
+		From string `nimona:"from,omitempty"`
+		// TODO: This should probably be a tilde.Map
+		Value DocumentMap `nimona:"value,omitempty"`
+	}
+)
+
