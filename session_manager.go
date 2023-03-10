3dff2d513ab0948cb0b4a6b4464ade5b7d25a40e
George Antoniadis <george@noodles.gr>
2023-03-10 19:39:09 +0000

    feat(session/manager): add LookupAlias, refactor Request to accept RequestRecipientFn
    BREAKING CHANGE: Request now accepts RequestRecipientFn as the final argument, FromAlias, FromIdentity, and FromPeerAddr can be used

--- session_manager.go
+++ session_manager.go
@@ -3,8 +3,11 @@ package nimona
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/hashicorp/golang-lru/v2/simplelru"
+
+	"nimona.io/internal/xsync"
 )
 
 // SessionManager manages the dialing and accepting of connections.
@@ -16,6 +19,11 @@ type SessionManager struct {
 	handlers   map[string]RequestHandlerFunc
 	publicKey  PublicKey
 	privateKey PrivateKey
+
+	resolver   Resolver
+	aliases    xsync.Map[IdentityAlias, *IdentityInfo]
+	providers  xsync.Map[IdentityAlias, *IdentityInfo]
+	identities xsync.Map[DocumentID, *IdentityInfo]
 }
 
 type RequestHandlerFunc func(context.Context, *Request) error
@@ -49,6 +57,7 @@ func NewSessionManager(
 		publicKey:  publicKey,
 		privateKey: privateKey,
 		handlers:   map[string]RequestHandlerFunc{},
+		resolver:   &ResolverHTTP{},
 	}
 
 	if listener != nil {
@@ -103,7 +112,81 @@ func (cm *SessionManager) connCacheKey(k PublicKey) connCacheKey {
 	}
 }
 
+type RequestRecipientFn func(*requestRecipient)
+
+type requestRecipient struct {
+	Alias    *IdentityAlias
+	Identity *Identity
+	PeerAddr *PeerAddr
+}
+
+func FromAlias(alias IdentityAlias) RequestRecipientFn {
+	return func(r *requestRecipient) {
+		r.Alias = &alias
+	}
+}
+
+func FromIdentity(identity Identity) RequestRecipientFn {
+	return func(r *requestRecipient) {
+		r.Identity = &identity
+	}
+}
+
+func FromPeerAddr(peerAddr PeerAddr) RequestRecipientFn {
+	return func(r *requestRecipient) {
+		r.PeerAddr = &peerAddr
+	}
+}
+
 func (cm *SessionManager) Request(
+	ctx context.Context,
+	req *Document,
+	rfn RequestRecipientFn,
+) (*Response, error) {
+	rec := &requestRecipient{}
+	rfn(rec)
+
+	switch {
+	case rec.Alias != nil:
+		return cm.requestFromAlias(ctx, *rec.Alias, req)
+	case rec.Identity != nil:
+		return cm.requestFromIdentity(ctx, rec.Identity, req)
+	case rec.PeerAddr != nil:
+		return cm.requestFromPeerAddr(ctx, *rec.PeerAddr, req)
+	default:
+		return nil, fmt.Errorf("no recipient specified")
+	}
+}
+
+func (cm *SessionManager) requestFromAlias(
+	ctx context.Context,
+	alias IdentityAlias,
+	req *Document,
+) (*Response, error) {
+	// resolve the alias
+	info, err := cm.LookupAlias(alias)
+	if err != nil {
+		return nil, fmt.Errorf("error looking up alias %s: %w", alias, err)
+	}
+
+	return cm.requestFromIdentity(ctx, &info.Identity, req)
+}
+
+func (cm *SessionManager) requestFromIdentity(
+	ctx context.Context,
+	identity *Identity,
+	req *Document,
+) (*Response, error) {
+	// resolve the identity
+	info, err := cm.LookupIdentity(NewDocumentID(identity.Document()))
+	if err != nil {
+		return nil, fmt.Errorf("error looking up identity %s: %w", identity, err)
+	}
+
+	return cm.requestFromPeerAddr(ctx, info.PeerAddresses[0], req)
+}
+
+func (cm *SessionManager) requestFromPeerAddr(
 	ctx context.Context,
 	addr PeerAddr,
 	req *Document,
@@ -210,4 +293,71 @@ func (cm *SessionManager) Close() error {
 	// purge will close all connections in the cache
 	cm.connCache.Purge()
 	return nil
+}
+
+func (cm *SessionManager) LookupAlias(alias IdentityAlias) (*IdentityInfo, error) {
+	if info, ok := cm.aliases.Load(alias); ok {
+		return info, nil
+	}
+
+	identityInfo, err := cm.resolver.ResolveIdentityAlias(alias)
+	if err != nil {
+		return nil, fmt.Errorf("unable to resolve provider alias: %w", err)
+	}
+
+	cm.aliases.Store(alias, identityInfo)
+	cm.identities.Store(NewDocumentID(identityInfo.Identity.Document()), identityInfo)
+
+	// TODO add recursive lookup for user identities
+	if identityInfo.Identity.Use == "provider" {
+		cm.providers.Store(alias, identityInfo)
+	}
+
+	return identityInfo, nil
+}
+
+func (cm *SessionManager) LookupIdentity(identityID DocumentID) (*IdentityInfo, error) {
+	if info, ok := cm.identities.Load(identityID); ok {
+		return info, nil
+	}
+
+	var identityInfo *IdentityInfo
+	cm.providers.Range(func(key IdentityAlias, providerInfo *IdentityInfo) bool {
+		ctx, cf := context.WithTimeout(context.Background(), time.Second)
+		defer cf()
+
+		for _, addr := range providerInfo.PeerAddresses {
+			rctx := &RequestContext{}
+			doc, err := RequestDocument(ctx, rctx, cm, identityID, FromPeerAddr(addr))
+			if err != nil {
+				continue
+			}
+
+			err = identityInfo.FromDocument(doc)
+			if err != nil {
+				continue
+			}
+
+			return false
+		}
+
+		return true
+	})
+
+	if identityInfo == nil {
+		return nil, fmt.Errorf("unable to resolve identity %s", identityID)
+	}
+
+	cm.identities.Store(identityID, identityInfo)
+
+	// TODO verify the alias is indeed correct before storing or returning it
+	if identityInfo.Alias.Hostname != "" {
+		cm.aliases.Store(identityInfo.Alias, identityInfo)
+	}
+
+	if identityInfo.Identity.Use == "provider" {
+		cm.providers.Store(identityInfo.Alias, identityInfo)
+	}
+
+	return identityInfo, nil
 }
