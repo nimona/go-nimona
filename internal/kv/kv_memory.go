3d7d29d19f4cfcde7c42fe900ec3e010423dbb59
George Antoniadis <george@noodles.gr>
2023-03-29 19:49:38 +0100

    feat(internal/kv): add memory store

---
+++ internal/kv/kv_memory.go
@@ -0,0 +1,42 @@
+package kv
+
+import (
+	"strings"
+
+	"nimona.io/internal/xsync"
+)
+
+func NewMemoryStore[K, V any]() (Store[K, V], error) {
+	return &Memory[K, V]{
+		items: &xsync.Map[string, V]{},
+	}, nil
+}
+
+type Memory[K, V any] struct {
+	items *xsync.Map[string, V]
+}
+
+func (s *Memory[K, V]) Set(key K, value *V) error {
+	s.items.Store(keyToString(key), *value)
+	return nil
+}
+
+func (s *Memory[K, V]) Get(key K) (*V, error) {
+	value, ok := s.items.Load(keyToString(key))
+	if !ok {
+		return nil, nil
+	}
+	return &value, nil
+}
+
+func (s *Memory[K, V]) GetPrefix(key K) ([]*V, error) {
+	prefix := keyToString(key)
+	values := []*V{}
+	s.items.Range(func(k string, v V) bool {
+		if strings.HasPrefix(k, prefix) {
+			values = append(values, &v)
+		}
+		return true
+	})
+	return values, nil
+}
---
+++ internal/kv/kv_memory_test.go
@@ -0,0 +1,104 @@
+package kv
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_MemoryStore(t *testing.T) {
+	type TestValue struct {
+		Foo string `json:"foo"`
+	}
+
+	// Create a new Memory instance
+	store, err := NewMemoryStore[string, TestValue]()
+	assert.NoError(t, err)
+
+	// Set a new key-value pair with a struct as value
+	testValue := &TestValue{Foo: "bar"}
+	err = store.Set("structValue", testValue)
+	assert.NoError(t, err)
+
+	// Get the value back and check that it matches the original value
+	returnedValue, err := store.Get("structValue")
+	assert.NoError(t, err)
+	assert.Equal(t, testValue, returnedValue)
+}
+
+func Test_MemoryStorePrefix(t *testing.T) {
+	type testKey struct {
+		Foo string
+		Bar string
+	}
+	type testPair struct {
+		key   testKey
+		value string
+	}
+	tests := []struct {
+		name    string
+		insert  []testPair
+		prefix  testKey
+		results []string
+	}{{
+		name: "simple",
+		insert: []testPair{{
+			key:   testKey{Foo: "foo", Bar: "bar"},
+			value: "value-for-foo-bar",
+		}, {
+			key:   testKey{Foo: "foo", Bar: "baz"},
+			value: "value-for-foo-baz",
+		}, {
+			key:   testKey{Foo: "not-foo", Bar: "qux"},
+			value: "value-for-not-foo-qux",
+		}},
+		prefix: testKey{Foo: "foo"},
+		results: []string{
+			"value-for-foo-bar",
+			"value-for-foo-baz",
+		},
+	}, {
+		name: "exact",
+		insert: []testPair{{
+			key:   testKey{Foo: "foo", Bar: "bar"},
+			value: "value-for-foo-bar",
+		}, {
+			key:   testKey{Foo: "foo", Bar: "baz"},
+			value: "value-for-foo-baz",
+		}},
+		prefix: testKey{Foo: "foo", Bar: "bar"},
+		results: []string{
+			"value-for-foo-bar",
+		},
+	}, {
+		name: "empty",
+		insert: []testPair{{
+			key:   testKey{Foo: "foo", Bar: "bar"},
+			value: "value-for-foo-bar",
+		}, {
+			key:   testKey{Foo: "foo", Bar: "baz"},
+			value: "value-for-foo-baz",
+		}},
+		prefix:  testKey{Foo: "not-foo"},
+		results: []string{},
+	}}
+	for _, test := range tests {
+		// Create a new Memory instance
+		store, err := NewMemoryStore[testKey, string]()
+		assert.NoError(t, err)
+
+		// Set a new key-value pair with a struct as value
+		for _, pair := range test.insert {
+			err = store.Set(pair.key, &pair.value)
+			assert.NoError(t, err)
+		}
+
+		// Get the value back and check that it matches the original value
+		returnedValues, err := store.GetPrefix(test.prefix)
+		assert.NoError(t, err)
+		assert.Len(t, returnedValues, len(test.results))
+		for _, result := range test.results {
+			assert.Contains(t, returnedValues, &result)
+		}
+	}
+}
